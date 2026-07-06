a1dabf99563fca3c26f986b9d30577650b090d80
Iacob-Mircea <iacobmircea21@gmail.com>
2026-07-06 20:10:58 +0300

    165

---
+++ Week1/Goal1/task63.c
@@ -0,0 +1,6 @@
+#include "iom324pb.h"
+
+//In fisierul task63.i se poate observa faptul ca macrourile sunt expandate in fisierul in care este inclus.
+//Da imi confirma ceea ce intelegeam eu din ele. Faptul ca acestea sunt reprezentate prin niste structuri pentru a fi folosite.
+
+void main(void){}
