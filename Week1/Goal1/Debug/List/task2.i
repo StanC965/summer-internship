fc1f43ba141f6dc5640d216b34d8e9c3e27eebdb
Iacob-Mircea <iacobmircea21@gmail.com>
2026-07-06 17:07:10 +0300

    153

---
+++ Week1/Goal1/Debug/List/task2.i
@@ -0,0 +1,14 @@
+#line 1 "D:\\Mircea\\Marqurdt\\summer-internship\\Week1\\Goal1\\task2.c"
+
+
+
+
+
+
+
+
+void main(void){
+  
+  int a = 4;
+
+}
---
+++ Week1/Goal1/task2.c
@@ -0,0 +1,13 @@
+
+
+#if MAX == 1
+#define VAR 4
+#else
+#define VAR 5
+#endif
+
+void main(void){
+  //VAR va fi 5, iar daca definim MAX = 1 atunci va fi 4
+  int a = VAR;
+
+}
