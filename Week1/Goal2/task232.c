cc60c63204f4d3ab7dd63927f1a8a01aa7992cec
Iacob-Mircea <iacobmircea21@gmail.com>
2026-07-07 11:47:26 +0300

    232

--- Week1/Goal2/task232.c
+++ Week1/Goal2/task232.c
@@ -4,11 +4,13 @@ void set_pin(volatile unsigned char* port,unsigned char pin){
     *port |= (1 << pin);
 }
 
-
+void reset_pin(volatile unsigned char* port,unsigned char pin){
+    *port &= ~(1<<pin);
+} 
 
 void main(void){
-
+  PORTC = 0xff;
   DDRC = 0xFF;
-  set_pin(&PORTC,7);
+  reset_pin(&PORTC,7);
   while(1){}
 }
