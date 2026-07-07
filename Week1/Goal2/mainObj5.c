7ebb873523ae1a56e66e4a0faa88c1f75dfe2099
Iacob-Mircea <iacobmircea21@gmail.com>
2026-07-07 14:54:17 +0300

    253

--- Week1/Goal2/mainObj5.c
+++ Week1/Goal2/mainObj5.c
@@ -6,12 +6,11 @@ int main( void )
   set_pin(&PORTC,6);
   set_direction(&DDRC,7,1);
   set_pin(&PORTC,7);
-  unsigned char sw;
-  unsigned char sum=0;
-  while(1){
-  sw = PINC;
   
-  if(!(sw & (1<<6))){
+  while(1){
+  
+  
+  if(!get_pin(&PINC,6)){
       reset_pin(&PORTC,7);
   }
   else{
