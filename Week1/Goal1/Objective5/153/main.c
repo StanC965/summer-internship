934ea7b5426e5bf4cfba8877c11febdffcc386e0
Sebi0411 <sebi041102@gmail.com>
2026-07-07 15:37:31 +0300

    153

---
+++ Week1/Goal1/Objective5/153/main.c
@@ -0,0 +1,10 @@
+#if MAX == 1
+#define VAR 4
+#else
+#define VAR 5
+#endif
+int main( void )
+{
+  int a = VAR;
+  return a;
+}
---
+++ Week1/Goal1/Objective5/153/main.i
@@ -0,0 +1,11 @@
+#line 1 "C:\\Users\\sebi0\\Documents\\main.c"
+
+
+
+
+
+int main( void )
+{
+  int a = 5;
+  return a;
+}
