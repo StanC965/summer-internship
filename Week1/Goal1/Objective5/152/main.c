ca15fc1e450859b4d1c3a85dd242d4040f1f6a0b
Sebi0411 <sebi041102@gmail.com>
2026-07-07 14:50:11 +0300

    152

---
+++ Week1/Goal1/Objective5/152/main.c
@@ -0,0 +1,8 @@
+  int sum = 0; // initialise sum
+  int a=5,b=2; // initialise variables
+int main( void )
+{
+  
+  sum = a + b; // compute and store result in sum
+  return sum;
+}
---
+++ Week1/Goal1/Objective5/152/main.i
@@ -0,0 +1,9 @@
+#line 1 "C:\\Users\\sebi0\\Documents\\main.c"
+  int sum = 0; 
+  int a=5,b=2; 
+int main( void )
+{
+  
+  sum = a + b; 
+  return sum;
+}
