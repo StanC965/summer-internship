8a174226f3af44b80a1690d0bfa7f3077b4fc5a5
Raluca Fracea <ralucafracea16@gmail.com>
2026-07-07 13:20:13 +0300

    158

--- Week 1/Goal1/Debug/List/main.i
+++ Week 1/Goal1/Debug/List/main.i
@@ -1,8 +1,19 @@
 #line 1 "C:\\MQ_Summer_Internship\\summer-internship\\Week 1\\Goal1\\main.c"
+volatile int ready=0; 
 
+void count(void){
+  static int contor=0; 
+  contor++;
+}
 
+void test(){
+  if(ready==0)
+    ready=1;
+}
 
 void main(void){
-   int a=(((2)>(3))? 2:3);; 
-   double b=(((5)+(7))/2.0);; 
+  count(); 
+  count(); 
+  test();
+    
 }
--- Week 1/Goal1/main.c
+++ Week 1/Goal1/main.c
@@ -1,7 +1,18 @@
-#define max(a,b) (((a)>(b))? a:b); //paranteza pentru fiecare parametru pentru a respecta ordinea operatiilor
-#define average(a,b) (((a)+(b))/2.0);
+volatile int ready=0; //poate fi schimbata de hardware
+
+void count(void){
+  static int contor=0; //se initializeaza o singura data, isi pastreaza valoarea intre apelurile functiei, dar nu si in afara acesteia
+  contor++;
+}
+
+void test(){
+  if(ready==0)
+    ready=1;
+}
 
 void main(void){
-   int a=max(2,3); // a=3
-   double b=average(5,7); //b=6
+  count(); //1
+  count(); //2
+  test();
+    
 }
