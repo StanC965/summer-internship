0a17b70c7dd28e3956660eaad524aba912df985e
Raluca Fracea <ralucafracea16@gmail.com>
2026-07-07 18:32:43 +0300

    167

--- Week 1/Goal1/main.c
+++ Week 1/Goal1/main.c
@@ -1,7 +1,6 @@
-//166
-#include <iom324pb.h>
+//167
+#include "iom328pb.h"
 
-void main(void){
-  //comentariu
-  PORTC=0xFF;
-}
+//UCSR0A SI TCCR0A pe ATmega328pb sunt la fel ca la ATmega324pb
+//PORTA este inexistent pe ATmega328pb din cauza lipsei pinului
+
