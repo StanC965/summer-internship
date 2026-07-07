d64ac17c79010f4d4b47afcaa2d25f675a9313ce
nelutuinfinite <81085177+nelutuinfinite@users.noreply.github.com>
2026-07-07 11:07:42 +0300

    Folder week 1 si Task 152

---
+++ week1/Goal 1/task1.c
@@ -0,0 +1,6 @@
+
+//suma a doua numere
+void main(void){
+ int a=2,b=3;
+ int sum = a+b;
+}
---
+++ week1/Goal 1/task2.c
@@ -0,0 +1,9 @@
+#if MAX == 1
+#define VAR 4
+#else
+#define VAR 5
+#endif
+
+void main(){
+int x =VAR;
+}
