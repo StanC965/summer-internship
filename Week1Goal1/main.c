e07f782caa750706fa0cf0c6194ac5281c18029c
bianca.cotofana <biancamaria.cotofana@ulbsibiu.ro>
2026-07-07 17:16:52 +0300

    153 - undefined macro treated as 0

--- Week1Goal1/main.c
+++ Week1Goal1/main.c
@@ -1,13 +1,13 @@
-// 143 - first IAR project, sum of two numbers
-// program adds 2 integer variables and stores the result 
+//153 
+#if MAX == 1
+#define VAR 4
+#else
+#define VAR 5
+#endif
+
 void main( void )
 {
-    int a = 5, b = 4;  //operands
-    int sum = 0;       // will hold the result
-    sum = a + b;       // the sum operation
-    
-    while(1)
-    {
-      //infinite loop so we can inspect the sum in debug mode
-    }
+    int test = VAR; 
 }
+
+// MAX is treated as 0 because it is not defined => test = var = 5
