1d58437995b3b078fd1817b16b9cce27a3372f6d
Raluca Fracea <ralucafracea16@gmail.com>
2026-07-07 12:30:53 +0300

    153

---
+++ Week 1/Goal1/Debug/List/main.i
@@ -0,0 +1,10 @@
+#line 1 "C:\\MQ_Summer_Internship\\summer-internship\\Week 1\\Goal1\\main.c"
+
+
+
+
+
+
+void main(){
+  int x=5; 
+}
--- Week 1/Goal1/main.c
+++ Week 1/Goal1/main.c
@@ -1,8 +1,9 @@
+#if MAX == 1
+#define VAR 4
+#else
+#define VAR 5
+#endif
 
-int main( void )
-{
-  //suma a doua numere
-  int a=2, b=2, sum=0;
-  sum=a+b;
-  return sum;
+void main(){
+  int x=VAR; //x=5 deci avem VAR 5 pentru ca MAX nu este 1
 }
