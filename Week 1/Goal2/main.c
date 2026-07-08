fd3d3649d3c34613c3918869f243dcc442fc2a25
Raluca Fracea <ralucafracea16@gmail.com>
2026-07-08 11:37:02 +0300

    216

--- Week 1/Goal2/main.c
+++ Week 1/Goal2/main.c
@@ -7,6 +7,12 @@ DDRC=0x80;
 
 void main(void){
   setup();
-  PORTC=0x7F;
-  while(1);
+  
+
+  while(1){
+    PORTC=0x7F;
+    for(volatile long i=0;i<200000;i++);
+    PORTC=0xFF;
+    for(volatile long i=0;i<200000;i++);
+  }
 }
