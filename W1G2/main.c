e046e5810a05d975e7dd2f7cecd4c5ed132806f5
unknown <stefanadrian.deac@ulbsibiu.ro>
2026-07-08 15:09:58 +0300

    EX 215  pornire LED0

--- W1G2/main.c
+++ W1G2/main.c
@@ -1,5 +1,15 @@
 #include "iom324pb.h"
+void setup()
+{
+  DDRC=0b10000000;
+    PORTC=0x7f;
+}
+// logica am scris-o la exercitiul anterior in fisierul txt
 void main( void )
 {
-  
+  setup();
+  while(1)
+  {
+    
+  }
 }
