a704423e41411f67e8d897bcc1be1c0bd5d6ad24
chirila viorel <viorelchirila21@yahoo.ro>
2026-07-08 21:58:59 +0300

    155 Code explanation

--- Week1Goal1/IAR_Project/Debug/List/main.i
+++ Week1Goal1/IAR_Project/Debug/List/main.i
@@ -1,8 +1,5 @@
 
 void main (void)
 {
-  int x=2;
-  x=55;
+  int 100 = 10;
 }
-
-
--- Week1Goal1/IAR_Project/main.c
+++ Week1Goal1/IAR_Project/main.c
@@ -1,10 +1,10 @@
-#define MAX 10
+#define MAX 100
 
 void main (void)
 {
-  int x=2;
-  #define MAX 55
-  x=MAX;
+  int MAX = 10;
 }
 
-// value of x will be 55 the new vlue of MAX
+// This code will generate an error MAX is replaced with the value 100 
+// This can be seen in the generated .i file, where the code becomes int 100 = 10; the MAX macro has been replaced with its value. 
+// As a result, the compiler encounters 100 where it expects a variable name, causing a compilation error.
