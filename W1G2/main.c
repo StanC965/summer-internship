cb1a160174268a0b81e96175ac90669ff2dd78c1
unknown <stefanadrian.deac@ulbsibiu.ro>
2026-07-08 17:10:04 +0300

    EX 221  identificare rezistor LED0

--- W1G2/main.c
+++ W1G2/main.c
@@ -8,6 +8,10 @@ void setup()
     TCCR1B = (1<<1) | (1<<0);  //Desi pe placuta apare 16Mhz ,initial la un prescale de 1024 imi statea 16 secunde
     DDRC = 0b10000000;         //asa ca am am impartit prescale-ul la 16 si am obtinut 64
     PORTC = 0xFF;              //acuma sta o secunda, dupa ce am configurat in TCCR1B ca sa fie 64
+    DDRD=(1<<5)|(1<<4);
+    PORTD=0b11001111;
+    DDRA=0b00001000;
+    PORTA=0xf7;
 }
 
 void main(void)
