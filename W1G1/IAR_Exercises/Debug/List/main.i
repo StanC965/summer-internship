b41de25b03c6b14991092581d4c9d8052c0d9545
unknown <stefanadrian.deac@ulbsibiu.ro>
2026-07-08 08:49:25 +0300

    EX 162 - determinare little endian

--- W1G1/IAR_Exercises/Debug/List/main.i
+++ W1G1/IAR_Exercises/Debug/List/main.i
@@ -1,48 +1,12 @@
 #line 1 "C:\\Users\\Stefan\\summer-internship\\W1G1\\IAR_Exercises\\main.c"
-
-
-int contor_apeluri(void)
-{
-    
-   
-    static int apeluri = 0; 
-    
-    apeluri++;
-    return apeluri;
-}
-
-
-
-volatile int senzor_hardware = 0; 
-
-void simulare_intrerupere_hardware(void)
-{
+int a=1,b=278;
+  int sum=0;
   
-    senzor_hardware++;
-}
 
 
-
-
-int main(void)
+int main (void)
 {
-    
-    static int x = 1; 
-    
-   
-    for(int i = 0; i < 3; i++)
-    {
-      
-        int nr_actual = contor_apeluri();
-       
-    }
-    
-
-    
-    
-    simulare_intrerupere_hardware(); 
-    
-
-    
-    return 0;
+  sum=a+b;
+  
+  return sum;
 }
--- W1G1/IAR_Exercises/main.c
+++ W1G1/IAR_Exercises/main.c
@@ -1,47 +1,11 @@
+int a=1,b=278;
+  int sum=0;
+  
 
 
-int contor_apeluri(void)
+int main (void)
 {
-    // O variabila statica locala este initializata o SINGURA data (la compilare).
-   
-    static int apeluri = 0; 
-    
-    apeluri++;
-    return apeluri;
-}
-
-//volatile ii interzice compilatorului sa faca optimizari precum stocarea variabile intr-un registru mai rapid
-//astfel de fiecare daca cand se citeste se va citi de la adresa de memorie propri-zisa pentru a se asigura ca nicio intrerupere nu a modificat valoarea.
-volatile int senzor_hardware = 0; 
-
-void simulare_intrerupere_hardware(void)
-{
-  //simulam un interrupt
-    senzor_hardware++;
-}
-
-
-
-
-int main(void)
-{
-    // 'static' aici limiteaza vizibilitatea lui 'x '  doar la acest fisier
-    static int x = 1; 
-    
-   
-    for(int i = 0; i < 3; i++)
-    {
-      
-        int nr_actual = contor_apeluri();
-       
-    }
-    
-
-    
-    
-    simulare_intrerupere_hardware(); 
-    
-
-    
-    return 0;
+  sum=a+b;
+  
+  return sum;
 }
