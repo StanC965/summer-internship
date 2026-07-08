ec28afc742d21ed1d8e91e933b803acfdccd50bd
unknown <stefanadrian.deac@ulbsibiu.ro>
2026-07-08 19:36:00 +0300

    EX 234 toggle pin

--- W1G2/main.c
+++ W1G2/main.c
@@ -21,22 +21,28 @@ void setup()
 
 void set_pinC(unsigned char bit)
 {
-    PORTC |= (1 << bit); 
+    PORTC |= (1 << bit);  //functia trebuie sa primeasca pin-ul care vrem sa fie setat 
     
 }
 
 
 void reset_pinC(unsigned char bit)
 {
-    PORTC &= ~(1 << bit); 
+  PORTC &= ~(1 << bit);                 //functia trebuie sa primeasca pin-ul care vrem sa fie resetat 
+  
     
+}
+
+void toggle_pinC(unsigned char bit)
+{
+  PORTC ^=1<<bit;             
 }
 
 void set_direction(unsigned char bit,_Bool intrare)
 {
   if(intrare)
-  DDRC&=~(1<<bit);
-  else
+  DDRC&=~(1<<bit);                              //spre deosebire de celelalte,functia asta trebuie sa aiba ca si parametru formal si un bool    
+  else                                 //care sa specifice daca pin-ul setat sa fie de intrare sau iesire in DDR;
  DDRC|=1<<bit;
   
 }
@@ -48,7 +54,7 @@ void main(void)
 
     while(1)
     {
-        
+                                                      //TOATE 3 functiile nu trebuie sa returneze nimic caci modifica PORTC si atat;
       
         if (TCNT1 >= OCR1A)   
         {
@@ -66,8 +72,8 @@ void main(void)
             TCNT1=0;
             counter++;
             if(counter==5)
-              
-            set_direction(7,1);
+              toggle_pinC(7); // observam ca la a 5a perioada se opreste LED-ul si dupa aceea REVINE, asta inseamna ca toggle-ul a functionat ;
+             // set_direction(7,1);
         }
     }
 }
