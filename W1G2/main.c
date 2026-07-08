5264550360e334114c869965d5f61cd0c5dccebb
unknown <stefanadrian.deac@ulbsibiu.ro>
2026-07-08 17:31:49 +0300

    EX 224 led-uri sequence

--- W1G2/main.c
+++ W1G2/main.c
@@ -3,27 +3,47 @@
 void setup()
 {
     TCNT1 = 0;
-    TCCR1A = 0;           //aici alegi fie normal fie ctc 
-    OCR1A = 15625;             
-    TCCR1B = (1<<1) | (1<<0);  //Desi pe placuta apare 16Mhz ,initial la un prescale de 1024 imi statea 16 secunde
-    DDRC = 0b10000000;         //asa ca am am impartit prescale-ul la 16 si am obtinut 64
-    PORTC = 0xFF;              //acuma sta o secunda, dupa ce am configurat in TCCR1B ca sa fie 64
+    TCCR1A = 0;           
+    OCR1A = 62500;             
+    TCCR1B = (1<<1) ;   // pentru jumate de secunda avem 0.5 * 1000000/prescale; l-am setat pe 8 deci avem 62500
+    DDRC = 0b10000000;         
+    PORTC = 0xFF;              
     DDRD=(1<<5)|(1<<4);
-    PORTD=0b11001111;
+    PORTD=0xFF;
     DDRA=0b00001000;
-    PORTA=0xf7;
+    PORTA=0xFF;
 }
 
+unsigned char sequence[6]={
+  
+  0b00010000,
+   0b00010000,
+   0b00100000,
+      0b00100000,                               //am facut un sequence care este incrementat de fiecare data cand trec 0.5 sec
+   0b00001000,
+   0b00001000
+};
+      unsigned char i=0;
 void main(void)
 {
     setup();
 
     while(1)
     {
+    
         if (TCNT1 >= OCR1A)
         {
-            PORTC ^= (1<<7);      //switch bit
+            if(i<4)
+            {
+              PORTD^=sequence[i++];
+            }
+            else if(i>=4)
+            {
+              PORTA^=sequence[i++];
+            }
             TCNT1 = 0;            //reset timer
+            if(i==6)                    //reset sequence
+              i=0;
         }
     }
 }
