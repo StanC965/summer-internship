a2cb07db58866da2c857bf7c037505d379d591dd
Strava Cosmin <kosmin.strava@gmail.com>
2026-07-08 16:30:31 +0300

    212_CORE + 213_CORE + 214_CORE

--- work/StravaC/week1/goal2/Goal2_Project/main.c
+++ work/StravaC/week1/goal2/Goal2_Project/main.c
@@ -1,5 +1,26 @@
 #include "iom324pb.h"
 
+/*
+212:
+PORTC are 8 pini: PC0, PC1, PC2, PC3, PC4, PC5, PC6 si PC7.
+
+213:
+Microcontroller-ul ATmega324PB are in total 5 porturi GPIO:
+PORTA, PORTB, PORTC, PORTD si PORTE.
+
+PORTA, PORTB, PORTC si PORTD sunt porturi pe 8 biti.
+PORTE este un port pe 7 biti: PE0 - PE6.
+
+214:
+Conform User Guide-ului si schematicii placii ATmega324PB Xplained Pro,
+LED0 este conectat la pinul PC7, adica bitul 7 din PORTC.
+
+Pentru a controla LED0, pinul PC7 trebuie configurat ca output folosind registrul DDRC.
+
+LED0 este active-low, ceea ce inseamna ca se aprinde atunci cand scriem 0 logic pe PC7
+si se stinge atunci cand scriem 1 logic pe PC7.
+*/
+
 void main(void)
 {
     while (1)
