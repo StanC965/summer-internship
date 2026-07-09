b1733bf7e1378ba1e532c20251c786d73b9f077c
Truica Robert <robertioan.truica@ulbsibiu.ro>
2026-07-09 14:23:50 +0300

    w1g2 - task 231: Setpin function

--- students/TruicaR/w1g2/main.c
+++ students/TruicaR/w1g2/main.c
@@ -1,27 +1,15 @@
 #include "iom324pb.h"
-
+void set_pin(unsigned char pin)
+{
+  PORTC = PORTC | (1<<pin);
+}
 void main(void)
 {
   
-    DDRD_DDRD5 = 1;
-    DDRD_DDRD4 = 1;
-    DDRA_DDRA3 = 1;
+
     while (1)
     { 
-      for(volatile int i=0; i<8000;i++){  
-        PORTD_PORTD5=0;}
-      for(volatile int i=0; i<8000;i++){
-        PORTD_PORTD5=1;}
-      for(volatile int i=0; i<8000;i++){
-        PORTD_PORTD4=0;}
-       for(volatile int i=0; i<8000;i++){
-         PORTD_PORTD4=1;}
-        for(volatile int i=0; i<8000;i++){
-        
-          PORTA_PORTA3=0;}
-         for(volatile int i=0; i<8000;i++){
-           PORTA_PORTA3=1;}
-         for(volatile int i=0; i<8000;i++){}
+  
         
     }
 }
