83d26520a5366130ac5a2bc8de1846c806713082
Strava Cosmin <kosmin.strava@gmail.com>
2026-07-09 15:18:12 +0300

    244_CORE

--- work/StravaC/week1/goal2/Goal2_Project/main.c
+++ work/StravaC/week1/goal2/Goal2_Project/main.c
@@ -2,6 +2,30 @@
 #include "gpio.h"
 #include "led.h"
 
+/*
+244:
+Constructiile de forma #ifndef, #define si #endif se numesc include guards.
+
+Rolul lor este sa protejeze fisierul header ca sa nu fie inclus de mai multe ori
+in acelasi fisier sursa.
+
+Daca gpio.h este inclus o singura data, continutul lui este preluat normal
+de preprocesor.
+
+Daca gpio.h este inclus de doua ori si are include guards,
+continutul lui apare doar o singura data in fisierul .i generat de preprocesor.
+
+Daca gpio.h este inclus de doua ori si nu are include guards,
+continutul lui apare de doua ori in fisierul .i.
+
+Asta poate produce probleme precum redefiniri de tipuri, redefiniri de constante
+sau declaratii duplicate.
+
+De aceea folosim include guards in fisierele .h.
+Ele fac codul mai sigur si permit includerea aceluiasi header din mai multe locuri
+fara erori de compilare.
+*/
+
 void main(void)
 {
     gpio_set_direction(&DDRC, 7, GPIO_OUTPUT);
