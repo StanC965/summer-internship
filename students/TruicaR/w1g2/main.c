4510c7f0b13e9b95b34378e1ae1eb2628632fc02
Truica Robert <robertioan.truica@ulbsibiu.ro>
2026-07-09 14:14:44 +0300

    w1g2 - task 224 : Turning all leds on and off after half a second (individually)

--- students/TruicaR/w1g2/main.c
+++ students/TruicaR/w1g2/main.c
@@ -8,8 +8,20 @@ void main(void)
     DDRA_DDRA3 = 1;
     while (1)
     { 
-         PORTD_PORTD5=0;
-         PORTD_PORTD4=0;
-         PORTA_PORTA3=0;
+      for(volatile int i=0; i<8000;i++){  
+        PORTD_PORTD5=0;}
+      for(volatile int i=0; i<8000;i++){
+        PORTD_PORTD5=1;}
+      for(volatile int i=0; i<8000;i++){
+        PORTD_PORTD4=0;}
+       for(volatile int i=0; i<8000;i++){
+         PORTD_PORTD4=1;}
+        for(volatile int i=0; i<8000;i++){
+        
+          PORTA_PORTA3=0;}
+         for(volatile int i=0; i<8000;i++){
+           PORTA_PORTA3=1;}
+         for(volatile int i=0; i<8000;i++){}
+        
     }
 }
