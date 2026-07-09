a44f7f671283edc890deb5cfa8df5b7d7949c230
Truica Robert <robertioan.truica@ulbsibiu.ro>
2026-07-09 15:16:24 +0300

    w1g2 - tasks 261,262: SOS

--- students/TruicaR/w1g2/main.c
+++ students/TruicaR/w1g2/main.c
@@ -1,17 +1,20 @@
 #include "iom324pb.h"
 #include "gpio.h"
 #include "led.h"
+#include "sos.h"
 
 void main(void)
 {
     set_direction(7, 1);   
+    set_direction(6, 0);  
+    set_pin(6);     
 
     while (1)
     {
-        PowerOn_LED(7);
-        for (volatile long i = 0; i < 800000; i++);
-
-        PowerOff_LED(7);
-        for (volatile long i = 0; i < 800000; i++);
+        if (debounce(6))
+        {
+            sos();
+            delay(1000000);  
+        }
     }
 }
---
+++ students/TruicaR/w1g2/sos.c
@@ -0,0 +1,33 @@
+#include "gpio.h"
+#include "led.h"
+#include "sos.h"
+
+#define LED0_PIN 7
+
+void delay(long units)
+{
+    for (volatile long i = 0; i < units; i++);
+}
+
+void point(void)
+{
+    PowerOn_LED(LED0_PIN);
+    delay(200000);
+    PowerOff_LED(LED0_PIN);
+    delay(200000);
+}
+
+void line(void)
+{
+    PowerOn_LED(LED0_PIN);
+    delay(600000);
+    PowerOff_LED(LED0_PIN);
+    delay(200000);
+}
+
+void sos(void)
+{
+    for (int i = 0; i < 3; i++) point();
+    for (int i = 0; i < 3; i++) line();
+    for (int i = 0; i < 3; i++) point();
+}
