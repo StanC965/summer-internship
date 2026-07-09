1f850791ae96732c33543532765d3e1eb76fb42d
Sebi0411 <sebi041102@gmail.com>
2026-07-09 15:40:11 +0300

    234

---
+++ Week1/Goal2/Objective3/234/main.c
@@ -0,0 +1,47 @@
+#include <iom324pb.h>
+
+#define INPUT  0
+#define OUTPUT 1
+
+void set_pin(unsigned char pin_number) 
+{
+    PORTC |= (1 << pin_number);
+}
+
+void reset_pin(unsigned char pin_number) 
+{
+    PORTC &= ~(1 << pin_number);
+}
+
+void set_direction(unsigned char pin_number, unsigned char direction) 
+{
+    if (direction == OUTPUT) 
+    {
+        DDRC |= (1 << pin_number);
+    } 
+    else 
+    {
+        DDRC &= ~(1 << pin_number);
+    }
+}
+
+void toggle_pin(unsigned char pin_number) 
+{
+    PORTC ^= (1 << pin_number);
+}
+
+void main() {
+  
+    set_direction(7, OUTPUT);
+    set_pin(7);
+        
+    while (1) 
+    {
+        
+      toggle_pin(7); 
+        
+      for (unsigned long i = 0; i < 350000; i++); 
+      
+    }
+  
+}
