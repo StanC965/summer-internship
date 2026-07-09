26690a621d92be7940ab9ddb8b472b143f9a49e9
Raluca Fracea <ralucafracea16@gmail.com>
2026-07-09 17:17:33 +0300

    331

--- Week 2/Goal 3/main.c
+++ Week 2/Goal 3/main.c
@@ -4,49 +4,72 @@
 #include "led.h"
 #include <intrinsics.h>
 
-#define PCIE2   2
-#define PCINT22 6
 
-volatile int button_state=0;
+#define PCIE2   2
+#define PCIE0   0
+#define BTN1_PIN 1      //pc1
+#define BTN2_PIN 0      //pa0
+#define BTN3_PIN 1      //pa1
+#define LED1_PIN 5      //pd5
+#define LED2_PIN 4      //pd4
+#define LED3_PIN 3      //pa3
+#define OUTPUT 1
+#define INPUT 0
 
 #pragma vector=PCINT2_vect
-__interrupt void Button_ISR(void) 
+__interrupt void PortC_ISR(void) 
 {
-    if(read_pin(&PINC,6)==0){
-        if(button_state==0){ //toggle la buton
-          button_state=1;
-        }
-        else {
-          button_state=0;
-        }
-        for(volatile long i=0; i<3000; i++);
-      }
+  if(read_pin(&PINC, BTN1_PIN)==0){
+    reset_pin(&PORTD,LED1_PIN);
+  }
+  else{
+    set_pin(&PORTD,LED1_PIN);
+  }
+}
+
+#pragma vector=PCINT0_vect
+__interrupt void PortA_ISR(void) 
+{
+  if(read_pin(&PINA, BTN2_PIN)==0){
+    reset_pin(&PORTD,LED2_PIN);
+  }
+  else{
+    set_pin(&PORTD,LED2_PIN);
+  }
+        
+  if(read_pin(&PINA, BTN3_PIN)==0){
+    reset_pin(&PORTA,LED3_PIN);
+  }
+  else{
+    set_pin(&PORTA,LED3_PIN);
+  }
+   
 }
 
 void setup(void) {
-    Init_LED(&DDRC, 7, 1);
-    PowerOff_LED(&PORTC, 7); 
-    set_direction(&DDRC, 6, 0);
-    set_pin(&PORTC, 6); 
+    Init_LED(&DDRD, LED1_PIN, OUTPUT);
+    Init_LED(&DDRD, LED2_PIN, OUTPUT);
+    Init_LED(&DDRA, LED3_PIN, OUTPUT);
+    set_pin(&PORTD, LED1_PIN); 
+    set_pin(&PORTD, LED2_PIN); 
+    set_pin(&PORTA, LED3_PIN); 
+    set_direction(&DDRC, BTN1_PIN, INPUT); 
+    set_direction(&DDRA, BTN2_PIN, INPUT);
+    set_direction(&DDRA, BTN3_PIN, INPUT);
+    set_pin(&PORTC, BTN1_PIN);
+    set_pin(&PORTA, BTN2_PIN);
+    set_pin(&PORTA, BTN3_PIN);
 
     // activam intreruperile pentru tot portul C
-    PCICR |= (1 << PCIE2);   
+    PCICR |= (1 << PCIE2) | (1 << PCIE0);   
     // pin 6
-    PCMSK2 |= (1 << PCINT22);
+    PCMSK2 |=(1<<BTN1_PIN ); //PORT C
+    PCMSK0 |=(1 << BTN2_PIN) | (1 << BTN3_PIN); //PORT A
     __enable_interrupt(); 
 }
 
 void main(void) {
     setup(); 
 
-    while(1){
-      if(button_state==1){
-        SOS(&PORTC,7);
-        PowerOff_LED(&PORTC, 7);
-        for(long i=0;i<300000;i++);
-      }
-      else{
-        PowerOff_LED(&PORTC, 7);
-      } 
-    }
+    while(1);
 }
