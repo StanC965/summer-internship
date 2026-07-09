26c71a42292ed07960dcb7f6c2c4905383f2bc80
Truica Robert <robertioan.truica@ulbsibiu.ro>
2026-07-09 14:33:02 +0300

    w1g2 - task 233: Set direction function with main testing

--- students/TruicaR/w1g2/main.c
+++ students/TruicaR/w1g2/main.c
@@ -3,10 +3,25 @@ void set_pin(unsigned char pin)
 {
   PORTC = PORTC | (1<<pin);
 }
+
+void reset_pin(unsigned char pin)
+{
+  PORTC = PORTC & ~(1<<pin);
+}
+
+void set_direction_pin(unsigned char pin, unsigned char direction)
+{
+  if (direction)
+        DDRC |= (1 << pin);
+    else
+        DDRC &= ~(1 << pin);
+}
+
 void main(void)
 {
   
-
+    set_direction_pin(7, 1);   
+    reset_pin(7); 
     while (1)
     { 
   
