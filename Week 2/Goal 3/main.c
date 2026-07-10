a46ff2d4e75ad9984a2d21a004882e2361944e45
Raluca Fracea <ralucafracea16@gmail.com>
2026-07-10 10:20:46 +0300

    333

--- Week 2/Goal 3/main.c
+++ Week 2/Goal 3/main.c
@@ -1,4 +1,4 @@
-//331
+//332
 #include "iom324pb.h"
 #include "gpio.h"
 #include "led.h"
@@ -7,9 +7,11 @@
 
 #define PCIE2   2
 #define PCIE0   0
+#define BTNSW0_PIN 6    //pc6
 #define BTN1_PIN 1      //pc1
 #define BTN2_PIN 0      //pa0
 #define BTN3_PIN 1      //pa1
+#define LED0_PIN 7      //pc7
 #define LED1_PIN 5      //pd5
 #define LED2_PIN 4      //pd4
 #define LED3_PIN 3      //pa3
@@ -23,6 +25,19 @@ volatile int stare_led3 = 0;
 #pragma vector=PCINT2_vect
 __interrupt void PortC_ISR(void) 
 { 
+  if(read_pin(&PINC, BTNSW0_PIN)==0){
+    reset_pin(&PORTC,LED0_PIN);
+    set_pin(&PORTD,LED1_PIN);
+    set_pin(&PORTD,LED2_PIN);
+    set_pin(&PORTA,LED3_PIN);
+    stare_led1=0;
+    stare_led2=0;
+    stare_led3=0;
+  }
+  else{
+    set_pin(&PORTC,LED0_PIN);
+  }
+  
   if(read_pin(&PINC, BTN1_PIN)==0){
     if(stare_led1==0){
       reset_pin(&PORTD,LED1_PIN);
@@ -72,20 +87,24 @@ void setup(void) {
     Init_LED(&DDRD, LED1_PIN, OUTPUT);
     Init_LED(&DDRD, LED2_PIN, OUTPUT);
     Init_LED(&DDRA, LED3_PIN, OUTPUT);
+    Init_LED(&DDRC, LED0_PIN, OUTPUT);
     set_pin(&PORTD, LED1_PIN); 
     set_pin(&PORTD, LED2_PIN); 
     set_pin(&PORTA, LED3_PIN); 
+    set_pin(&PORTC, LED0_PIN);
     set_direction(&DDRC, BTN1_PIN, INPUT); 
     set_direction(&DDRA, BTN2_PIN, INPUT);
     set_direction(&DDRA, BTN3_PIN, INPUT);
+    set_direction(&DDRC, BTNSW0_PIN, INPUT);
     set_pin(&PORTC, BTN1_PIN);
     set_pin(&PORTA, BTN2_PIN);
     set_pin(&PORTA, BTN3_PIN);
+    set_pin(&PORTC, BTNSW0_PIN);
 
     // activam intreruperile pentru tot portul C
     PCICR |= (1 << PCIE2) | (1 << PCIE0);   
     // pin 6
-    PCMSK2 |=(1<<BTN1_PIN ); //PORT C
+    PCMSK2 |=(1<<BTN1_PIN ) | (1<<BTNSW0_PIN); //PORT C
     PCMSK0 |=(1 << BTN2_PIN) | (1 << BTN3_PIN); //PORT A
     __enable_interrupt(); 
 }
