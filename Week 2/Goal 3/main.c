86e381e0b04e66a862d5f8f8c0bbb0f9d4412709
Raluca Fracea <ralucafracea16@gmail.com>
2026-07-10 13:13:59 +0300

    355

--- Week 2/Goal 3/main.c
+++ Week 2/Goal 3/main.c
@@ -1,4 +1,4 @@
-//353
+//355
 #include "iom324pb.h"
 #include <intrinsics.h>
 
@@ -9,17 +9,25 @@ __interrupt void ADC_interrupt(void){
   lumina_ambientala=ADCH;
 }
 
-void setup(){
+void adc_init(){
   ADMUX=0b01100100; //REFS[1:0]-AVcc with external capacitor at AREF pin, ADLAR=1 - left adjust, MUX[4:0] - ADC4=00100
   ADCSRA=0b10001000;  //ADEN=1 -enables the ADC, ADSC=0 - not starting the conversion, ADATE- no internal trigger signals, 
                   //ADIF=0 -no interrupt flag, ADIE=1 -use interrupts ,ADPS[2:0]=000 - no prescale
   __enable_interrupt();
 }
 
-void main(void){
-  setup();
-  while(1){
+void adc_start_conversion(void){
   ADCSRA=0b11001000;
+}
+
+unsigned char adc_get_result(void){
+  return lumina_ambientala;
+}
+
+void main(void){
+  adc_init();
+  while(1){
+    adc_start_conversion();
   }
 }
 
