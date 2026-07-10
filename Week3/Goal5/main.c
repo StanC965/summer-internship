6a0f74d2148aaf05930898aec301645a2f4b94e7
Iacob-Mircea <iacobmircea21@gmail.com>
2026-07-10 15:35:00 +0300

    -

--- Week3/Goal5/main.c
+++ Week3/Goal5/main.c
@@ -1,6 +1,28 @@
 //voi alege prescale 8 si OCR1A = 9999 pentru a avea exact 10 ms
+#include <intrinsics.h>
+#include "gpio.h"
+#include "led.h"
+
+#define WGM12 3
+#define CS12 2
+#define CS11 1
+#define CS10 0
+
+#pragma vector=TIMER1_COMPA_vect
+__interrupt void myInterrupt(void){
+}
 
 int main( void )
 {
-  return 0;
+  TCNT1 = 0;
+  OCR1A = 9999;
+  
+  setPin(&TCCR1B,WGM12);
+  
+  __enable_interrupt();
+  
+  
+  while(1){
+  
+  }
 }
