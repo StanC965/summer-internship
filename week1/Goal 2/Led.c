4d8470f2e63a663d0376802172180e503b4f0359
nelutuinfinite <81085177+nelutuinfinite@users.noreply.github.com>
2026-07-10 13:30:49 +0300

    Task 243

--- week1/Goal 2/Led.c
+++ week1/Goal 2/Led.c
@@ -7,3 +7,36 @@ void LedOn(volatile unsigned char *reg,PIN_Number pin){
 void LedOff(volatile unsigned char *reg,PIN_Number pin){
   set_pin(reg,pin);
 }
+void LedBlinkFast(volatile unsigned char *reg,PIN_Number pin){
+  LedOn(reg,pin);
+  delay(1);
+  LedOff(reg,pin);
+  delay(1);
+  LedOn(reg,pin);
+  delay(1);
+  LedOff(reg,pin);
+  delay(1);
+  LedOn(reg,pin);
+  delay(1);
+  LedOff(reg,pin);
+  delay(1);
+  LedOn(reg,pin);
+  delay(1);
+  LedOff(reg,pin);
+  delay(1);
+  LedOn(reg,pin);
+  delay(1);
+  LedOff(reg,pin);
+  delay(1);
+}
+void LedBlinkSlow(volatile unsigned char *reg,PIN_Number pin){
+  LedOn(reg,pin);
+  delay(2);
+  LedOff(reg,pin);
+  delay(2);
+  LedOn(reg,pin);
+  delay(2);
+  LedOff(reg,pin);
+  delay(2);
+  
+}
--- week1/Goal 2/Led.h
+++ week1/Goal 2/Led.h
@@ -2,3 +2,5 @@
 
 void LedOn(volatile unsigned char *reg,PIN_Number pin);
 void LedOff(volatile unsigned char *reg,PIN_Number pin);
+void LedBlinkFast(volatile unsigned char *reg,PIN_Number pin);
+void LedBlinkSlow(volatile unsigned char *reg,PIN_Number pin);
