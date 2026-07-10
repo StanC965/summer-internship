203d8469208ccf7e3b9954b4e9c95f4396caff84
Iacob-Mircea <iacobmircea21@gmail.com>
2026-07-10 21:01:00 +0300

    -

---
+++ Week3/Goal5/adc.c
@@ -0,0 +1,43 @@
+#ifndef ADC_C
+#define ADC_C
+#include "gpio.h"
+#define INPUT 0
+#define ADCS 6
+
+//aici sunt definite toate porturile care sunt folosite penrtu exercitii-le pe care le-am intampinat pana acum acum 
+//acestea se vor defini cu numarul bitului care va trebuii schimbat sau numele DDR sau portul care treuie folosit
+void initAdc(unsigned char* DDR,unsigned char* port,unsigned char pin,unsigned char ADMUXn,unsigned char REF0,unsigned char REF1,unsigned char ADLAR,unsigned char ADIE,unsigned char ADPS0,unsigned char ADPS1,unsigned char ADPS2){
+  setDirection(*DDR,pin,INPUT);
+  resetPin(*port,pin);
+  
+  setPin(&ADMUX,ADMUXn);
+  if(REF0 !=0)
+    setPin(&ADMUX,REF0);
+  if(REF1 != 0)
+    setPin(&ADMUX,REF1);
+  if(ADLAR != 0){
+    setPin(&ADMUX,ADLAR);
+  }
+  setPin(&ADCSRA,ADEN);
+  if(ADIE != 0)
+    setPin(&ADCSRA,ADIE);
+  if(ADPS0!= 0)
+      setPin(&ADCSRA,ADPS0);
+  if(ADPS1!= 0)
+      setPin(&ADCSRA,ADPS1);
+  if(ADPS2!= 0)
+      setPin(&ADCSRA,ADPS2);
+  
+  
+
+
+}
+
+
+void startConversionAdc(){
+  setPin(&ADCSRA,ADCS);
+
+}
+
+
+#endif
---
+++ Week3/Goal5/task362.c
@@ -0,0 +1,82 @@
+#include <intrinsics.h>
+#include "led.h"
+#include "gpio.h"
+#include "adc.h"
+
+#define PA3 3
+#define PD4 4
+#define PD5 5
+#define PA4 4
+#define ADC4 2
+#define ADC4DIDR 2
+#define REF0 6
+#define ADLAR 5
+#define ADIE 3
+#define ADPS00 0
+#define ADPS01 1
+#define ADPS02 2
+
+#define led0 0
+#define led1 1
+#define led2 2
+#define led3 3
+
+#define dark 120
+#define semiDark 80
+#define semiLight 40
+//curentul se masoara cu un ampermetru
+
+
+
+#pragma vector = ADC_vect
+__interrupt void myInterrupt(void){
+  unsigned char value = ADCH;
+  disableAdc();
+  
+  if(value < semiLight)
+  {
+    ledPowerOn(led1);
+      ledPowerOn(led2);
+      ledPowerOn(led3);
+  }
+  else
+    if(value >= semiLight && value < semiDark){
+      ledPowerOn(led1);
+      ledPowerOn(led2);
+      ledPowerOff(led3);
+    }
+  else
+    if(value >= semiDark && value < dark){
+      ledPowerOn(led1);
+      ledPowerOff(led2);
+      ledPowerOff(led3);
+    }
+    else{
+    ledPowerOff(led1);
+      ledPowerOff(led2);
+      ledPowerOff(led3);
+    }
+  enableAdc();
+  startConversionAdc();
+      
+}
+
+
+
+
+void main( void )
+{
+  DIDR0 |= (1 << ADC4DIDR);
+  initAdc(&DDRA,&PORTA,PA4,ADC4,REF0,ADLAR,0,ADIE,ADPS00,0,0);
+  ledInit(&DDRA,&PORTA,PA3);
+  ledInit(&DDRD,&PORTD,PD4);
+  ledInit(&DDRD,&PORTD,PD5);
+  enableAdc();
+  startConversionAdc();
+  
+  __enable_interrupt();
+  while(1){
+  
+  }
+  
+}
