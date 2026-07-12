2f52a83cd450cf4f23cc05610270fd66dda3480d
Cristina <cristina.saman@yahoo.ro>
2026-07-13 00:39:51 +0300

    objective 5: integrate button functionalities in main code for task 251

--- code/src/main.c
+++ code/src/main.c
@@ -6,53 +6,20 @@
 void main (void){
 
   led_init();
+  button_init();
+  button_enable_pullup(BUTTON_ONBOARD);
   
   while(1){
-    // INITIAL STATE:
-    led_power_off(LED_ONBOARD);
-    led_power_off(LED_OLED1_1);
-    led_power_off(LED_OLED1_2);
-    led_power_off(LED_OLED1_3);
-    led_power_off(LED_IO1);
-    delay(SECOND / 2);
-
-    // STATE 1
-    led_power_on(LED_OLED1_1);
-    delay(SECOND / 2);
-
-    // STATE 2
-    led_power_off(LED_OLED1_1);
-    led_power_on(LED_OLED1_2);
-    delay(SECOND / 2);
-
-    // STATE 3
-    led_power_off(LED_OLED1_2);
-    led_power_on(LED_OLED1_3);
-    delay(SECOND / 2);
-
-    // STATE 4
-    led_power_off(LED_OLED1_3);
-    led_power_on(LED_ONBOARD);
-    delay(SECOND / 2);
-
-    // STATE 5
-    led_power_off(LED_ONBOARD);
-    led_power_on(LED_IO1);
-    delay(SECOND / 2);
+    button_pressed = button_read(BUTTON_ONBOARD);
     
-    // STATE 6
-    led_power_off(LED_IO1);
-    led_toggle(LED_ONBOARD);
-    delay(SECOND / 2);
-    
-    // STATE 7
-    led_power_off(LED_ONBOARD);
-    led_blink_fast(LED_IO1);
-    delay(SECOND / 2);
-    
-    // STATE 8
-    led_blink_slow(LED_OLED1_1);
-    delay(SECOND / 2);
+    if(!button_pressed){
+      // Button is pressed, turn on the onboard LED
+      led_power_on(LED_ONBOARD); 
+    }
+    else{
+      // Button is not pressed, turn off the onboard LED
+      led_power_off(LED_ONBOARD); 
+    }
     
   }
   
