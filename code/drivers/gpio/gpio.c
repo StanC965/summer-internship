bf9adb0eb8cb1a86d1fbd1327f7c1a255c2850c0
Cristina <cristina.saman@yahoo.ro>
2026-07-13 00:38:23 +0300

    objective 5: add button module for task 251

--- code/drivers/gpio/gpio.c
+++ code/drivers/gpio/gpio.c
@@ -9,20 +9,24 @@
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Implementation
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-void gpio_set_pin(volatile uint8_t *port, uint8_t pin){
-  *port |= (1 << pin);
+void gpio_set_pin(volatile uint8_t *port_register, uint8_t pin){
+  *port_register |= (1 << pin);
 }
 
-void gpio_reset_pin(volatile uint8_t *port, uint8_t pin){
-  *port &= ~(1 << pin);
+void gpio_reset_pin(volatile uint8_t *port_register, uint8_t pin){
+  *port_register &= ~(1 << pin);
 }
 
 void gpio_set_direction(volatile uint8_t *ddr_register, uint8_t direction, uint8_t pin){
   *ddr_register = (direction == GPIO_OUTPUT) ? (*ddr_register | (1 << pin)) : (*ddr_register & ~(1 << pin)); 
 }
 
-void gpio_toggle_pin(volatile uint8_t *port, uint8_t pin){
-  *port ^= (1 << pin);
+void gpio_toggle_pin(volatile uint8_t *port_register, uint8_t pin){
+  *port_register ^= (1 << pin);
+}
+  
+uint8_t gpio_read_pin(volatile uint8_t *pin_register, uint8_t pin){
+  return (*pin_register & (1 << pin));
 }
 
 #endif /* GPIO_C */
