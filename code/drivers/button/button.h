db2ccf86a0bb2fb28b443486ac5bf3d29a65c5d7
Cristina <cristina.saman@yahoo.ro>
2026-07-13 00:38:57 +0300

    objective 5: add doxygen comments for button module for task 251

---
+++ code/drivers/button/button.h
@@ -0,0 +1,55 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Includes
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+#include <stdint.h>
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Exported types and values
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+/** \brief  Button identifier type */
+typedef enum {
+    // ATmega328P onboard button
+    BUTTON_ONBOARD = 0,
+
+    // OLED1 buttons
+    BUTTON_OLED1_1,
+    BUTTON_OLED1_2,
+    BUTTON_OLED1_3,
+
+    BUTTON_COUNT // Total number of buttons
+} button_id_t;
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Public functions
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+/** \fn     void button_init(void)
+
+    \brief      [ Initializes the button module. ]
+    \param[in]  [ None ]
+    \param[out] [ None ]
+    \return     [ None ]
+*/
+extern void button_init(void);
+
+/** \fn     void button_enable_pullup(button_id_t button_id)
+
+    \brief      [ Enables the pull-up resistor for the specified button. ]
+    \param[in]  button_id [ the button identifier ]
+    \param[out] [ None ]
+    \return     [ None ]
+*/
+extern void button_enable_pullup(button_id_t button_id);
+
+/** \fn     uint8_t button_read(button_id_t button_id)
+
+    \brief      [ Reads the state of the specified button. ]
+    \param[in]  button_id [ the button identifier ]
+    \param[out] [ None ]
+    \return     [ The state of the button (1 for pressed, 0 for not pressed) ]
+*/
+extern uint8_t button_read(button_id_t button_id);
+
+#endif /* BUTTON_H */
