f2a498d31cc857d2607f112e714b43924147b3fb
Cristina <cristina.saman@yahoo.ro>
2026-07-12 13:33:20 +0300

    fix objective 1: use correct counter variable type in code to avoid overflow

--- code/main.c
+++ code/main.c
@@ -1,17 +1,22 @@
 #include <iom324pb.h>
 
+void delay(long count){
+  for(long i = 0; i < count; i++);
+}
+
 void main (void){
   
-  DDRD |= 1 << 5;
-  DDRD |= 1 << 4;
-  DDRA |= 1 << 3;
-  
-  PORTD &= ~(1 << 5);
-  PORTD &= ~(1 << 4);
-  PORTA &= ~(1 << 3);
+  // set PC as OUTPUT by setting bit 7 to 1
+  DDRC |= 1 << 7;
   
   while(1){
+    // drive PC7 LOW by clearing bit 7 to 0
+    PORTC &= ~(1 << 7);
+    delay(100000); // 2000 000
     
+    // drive PC7 LOW by setting bit 7 to 1
+    PORTC |= 1 << 7;
+    delay(100000);
   }
   
 }
