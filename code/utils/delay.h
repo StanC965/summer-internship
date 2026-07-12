e8f6342774f564864c9c08b849d5211f37d0a9ed
Cristina <cristina.saman@yahoo.ro>
2026-07-12 23:03:13 +0300

    objective 4: add doxygen comments for delay module for task 243

--- code/utils/delay.h
+++ code/utils/delay.h
@@ -1,6 +1,19 @@
 #ifndef DELAY_H
 #define DELAY_H
 
-void delay(long count);
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Exported types and values
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+/** \brief  Module specific constant number zero */
+#define SECOND  (100000U)
+
+/** \fn     void delay(long count)
+
+    \brief      [ Introduces a delay of the specified duration. ]
+    \param[in]  count [ the delay duration ]
+    \param[out] [ None ]
+    \return     [ None ]
+*/
+extern void delay(long count);
 
 #endif /* DELAY_H */
