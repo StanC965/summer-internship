7884b2a75b6e0d7ec974ce662c02597b8f35d422
Raluca Fracea <ralucafracea16@gmail.com>
2026-07-13 14:30:40 +0300

    431-432

--- Week 2/Goal 4/main.c
+++ Week 2/Goal 4/main.c
@@ -1,66 +1,43 @@
-//427
+//431-432
+//Ambele leduri clipesc sincron prin resetarea pinilor la initializare
+//(1024*128)/1M=0.131072 s
+// putem folosi alti factori de prescalare, si inlocuim in formula 1024 cu 256,64,8 sau 1
 #include "iom324.h"
 #include "led.h"
 #include "gpio.h"
 #include <intrinsics.h>
 
+#define OC0A_PIN 3 //pb3 - LED-ul de pe placa IO1
 #define LED0_PIN 7 //pc7
 #define SW0_PIN 6 //pc6
 #define OUTPUT 1
 #define INPUT 0
 
-const unsigned char prescale[5]={
-  0b00000001, //1
-  0b00000010, //8
-  0b00000011, //64
-  0b00000100, //256
-  0b00000101, //1024
-};
-
-volatile unsigned char viteza_prescale=4;
-
 void timer0_init(void){
-  TCCR0A=0b00000000; //COM0A[1:0]- no compare mode, COM0B[1:0]- normal mode operation, 0x00 , WGM0[1:0]-normal mode operation
-  TIMSK0=0b00000001; //Overflow Interrupt Enable
-  TCCR0B=prescale[viteza_prescale];
+  TCCR0A=0b01000010; //COM0A[1:0]=01, COM0B[1:0]- normal mode operation, 0x00 , WGM0[1:0]=10 -toggle OC0A on compare match
+  OCR0A=127;
+  TIMSK0=0b00000010; //Overflow Interrupt Enable
+  TCCR0B=0b00000101;
   __enable_interrupt();
 }
 
-void led0_init(void){
+void leds_init(void){
   Init_LED(&DDRC,LED0_PIN,OUTPUT);
-  set_pin(&PORTC,LED0_PIN);
+  reset_pin(&PORTC,LED0_PIN);
+  set_direction(&DDRB,OC0A_PIN,OUTPUT);
+  reset_pin(&PORTB,OC0A_PIN);
+  
 }
 
-void sw0_init(void){
-  set_direction(&DDRC,SW0_PIN,INPUT);
-  set_pin(&PORTC,SW0_PIN);
-}
 
-#pragma vector=TIMER0_OVF_vect
-__interrupt void timer0_ovf_interrupt(void){
+#pragma vector=TIMER0_COMPA_vect
+__interrupt void timer0_compa_interrupt(void){
     Toggle_LED(&PORTC,LED0_PIN);
 
 }
 void main( void )
 {
-  led0_init();
-  sw0_init();
+  leds_init();
   timer0_init();
-  unsigned char buton_vechi=1;
-  while(1){
-    unsigned char buton_curent=read_pin(&PINC,SW0_PIN);
-    if(buton_vechi==1 && buton_curent==0){
-      for(volatile int i=0; i<3000; i++); //debounce
-      
-      if(read_pin(&PINC, SW0_PIN) == 0){
-        viteza_prescale++;
-        if(viteza_prescale > 4){
-        viteza_prescale = 0;
-        }
-        TCCR0B=prescale[viteza_prescale];
-      }
-    
-   }
-   buton_vechi=buton_curent;
-  }
+  while(1);
 }
