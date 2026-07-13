6d7be572798aa87b9ec9098bf951485aae1f6f08
Iacob-Mircea <iacobmircea21@gmail.com>
2026-07-13 21:04:19 +0300

    656

---
+++ Week3/Goal6/Debug/List/task656.i
@@ -0,0 +1,16 @@
+#line 1 "D:\\Mircea\\Marqurdt\\summer-internship\\Week3\\Goal6\\task656.c"
+
+  
+    
+    
+
+
+void main(void){
+
+  
+  
+  
+ unsigned char bm = 0;
+  bm = bm |1<<7 | bm | 1<< 5;
+  
+}
---
+++ Week3/Goal6/task656.c
@@ -0,0 +1,17 @@
+//typedef struct {
+  //  unsigned char flag1 : 1; 
+    //unsigned char flag2 : 1; 
+    //unsigned char value : 6; 
+//} BitFieldStruct;
+
+void main(void){
+// BitFieldStruct bf = {1,0,0};
+  
+  
+  
+ unsigned char bm = 0;
+  bm = bm |1<<7 ;
+  
+}
+
+//bitmaskul este mai eficient si din punct de vedere al memoriei si al numarului de instructiuni
