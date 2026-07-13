12967b78ce12c8c166d053d2129b54efa018c757
nelutuinfinite <81085177+nelutuinfinite@users.noreply.github.com>
2026-07-13 10:10:48 +0300

    Task 316

--- week1/Goal 2/button.c
+++ week1/Goal 2/button.c
@@ -1,7 +1,15 @@
 #include "button.h"
 
-void PressToSos(){
+
+__interrupt void buton_apasat(void)
+{
   if(PINC_PINC6==0){
+ btn ^= 1;
+  }
+}
+
+void PressToSos(){
+  if(btn==1){
      Point(&PORTC,PIN7);
      Point(&PORTC,PIN7);
      Point(&PORTC,PIN7);
@@ -12,48 +20,49 @@ void PressToSos(){
      Point(&PORTC,PIN7);
      Point(&PORTC,PIN7);
      delay(5);
+     btn=0;
   }
   }
 void SosStop(){
-   if(PINC_PINC6==0){
+   if(btn==1){
      delay(2);
      while(1){
-       if(PINC_PINC6==0){
-         delay(2);
+       if(btn==0){
          break;}
      Point(&PORTC,PIN7);
-     if(PINC_PINC6==0){
-       delay(2);
+     if(btn==0){
+       
        break;}
      Point(&PORTC,PIN7);
-     if(PINC_PINC6==0){
-         delay(2);
+     if(btn==0){
+         
          break;}
      Point(&PORTC,PIN7);
-    if(PINC_PINC6==0){
-         delay(2);
+    if(btn==0){
+         
          break;}
      Line(&PORTC,PIN7);
-     if(PINC_PINC6==0){
-         delay(2);
+     if(btn==0){
+        
          break;}
      Line(&PORTC,PIN7);
-     if(PINC_PINC6==0)
+     if(btn==0)
          break;
      Line(&PORTC,PIN7);
-     if(PINC_PINC6==0){
-         delay(2);
+     if(btn==0){
+         
          break;}
      Point(&PORTC,PIN7);
-     if(PINC_PINC6==0){
-         delay(2);
+     if(btn==0){
+         
          break;}
      Point(&PORTC,PIN7);
-    if(PINC_PINC6==0){
-         delay(2);
+    if(btn==0){
+         
          break;}
      Point(&PORTC,PIN7);
      
      }
+     btn=0;
    }
 }
--- week1/Goal 2/button.h
+++ week1/Goal 2/button.h
@@ -1,3 +1,8 @@
 #include "sos.h"
+#include "iom324pb.h"
 
+static volatile int btn=0;
+#pragma vector=PCINT2_vect
+__interrupt void buton_apasat(void);
 void PressToSos();
+void SosStop();
