3c561a5a52a956df20074c968cf956e9c0ca7aa5
Cristina <cristina.saman@yahoo.ro>
2026-07-13 13:22:15 +0300

    objective 6: implement sos play at button press for task 262

--- code/src/main.c
+++ code/src/main.c
@@ -2,6 +2,7 @@
 #include "led.h"
 #include "button.h"
 #include "sos.h"
+#include "delay.h"
 
 void main (void){
 
@@ -9,8 +10,23 @@ void main (void){
   button_init();
   button_enable_pullup(BUTTON_ONBOARD);
   
-  while(1){
-    sos_play(LED_ONBOARD);
-  }
+  uint8_t button_pressed;
   
+  while(1){
+    button_pressed = button_read(BUTTON_ONBOARD);
+    
+    if(!button_pressed){
+      delay(10 * MILISECOND);
+      
+      button_pressed = button_read(BUTTON_ONBOARD);
+      
+      if(!button_pressed){
+        while(1){
+          sos_play(LED_ONBOARD);
+          delay(SECOND);
+      
+        }
+      }
+    }
+  }
 }
