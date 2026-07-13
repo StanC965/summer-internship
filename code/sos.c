44956737d05fd647a786b02745f3b36f089c7cc8
Cristina <cristina.saman@yahoo.ro>
2026-07-13 13:21:04 +0300

    objective 6: define time unit for sos spacing

--- code/sos.c
+++ code/sos.c
@@ -7,15 +7,17 @@
 #include "led.h"
 #include "delay.h"
 
+#define TIME_UNIT (SECOND / 2U)
+
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Static private functions
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 static void sos_point(led_id_t led_id){
-    led_blink_custom(led_id, 1, SECOND / 2, SECOND / 2);
+    led_blink_custom(led_id, 1, TIME_UNIT, TIME_UNIT);
 }
 
 static void sos_line(led_id_t led_id){
-    led_blink_custom(led_id, 1, SECOND * 2, SECOND / 2);
+    led_blink_custom(led_id, 1, 3 * TIME_UNIT, TIME_UNIT);
 }
 
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -26,17 +28,18 @@ void sos_play(led_id_t led_id){
     sos_point(led_id);
     sos_point(led_id);
 
-    delay(SECOND);
+    delay(2 * TIME_UNIT);
 
     sos_line(led_id);
     sos_line(led_id);
     sos_line(led_id);
 
-    delay(SECOND);
+    delay(2 * TIME_UNIT);
     
     sos_point(led_id);
     sos_point(led_id);
     sos_point(led_id);
+   
 }
 
 #endif /* SOS_C */
