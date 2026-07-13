fa6e0fdafc07181783474b4a07b9f32e602457a7
Raluca Fracea <ralucafracea16@gmail.com>
2026-07-13 10:47:43 +0300

    363

--- Week 2/Goal 3/main.c
+++ Week 2/Goal 3/main.c
@@ -1,4 +1,4 @@
-//362
+//363
 #include "iom324pb.h"
 #include "led.h"
 #include "gpio.h"
@@ -11,22 +11,25 @@
 #define OUTPUT 1
 #define FULL_DARK 255
 #define SEMI_DARK 225
-#define SEMI_LIGHT 98
-#define FULL_LIGHT 30
+#define SEMI_LIGHT 120
+#define FULL_LIGHT 60
 
 volatile unsigned char lumina_ambientala=0;
 
 #pragma vector=ADC_vect
 __interrupt void ADC_interrupt(void){
   lumina_ambientala=ADCH;
+  ADCSRA = 0b00001000; //disable the ADC
 }
 
 void adc_init(){
   ADMUX=0b01100100; //REFS[1:0]-AVcc with external capacitor at AREF pin, ADLAR=1 - left adjust, MUX[4:0] - ADC4=00100
-  ADCSRA=0b10001000;  //ADEN=1 -enables the ADC, ADSC=0 - not starting the conversion, ADATE- no internal trigger signals, 
+  ADCSRA=0b00001000;  //ADEN=0 -disables the ADC, ADSC=0 - not starting the conversion, ADATE- no internal trigger signals, 
                   //ADIF=0 -no interrupt flag, ADIE=1 -use interrupts ,ADPS[2:0]=000 - no prescale
+  DIDR0=0b00010000; // disable ADC4D
   __enable_interrupt();
 }
+
 
 void oled1_leds_init(){
   Init_LED(&DDRD, LED1_PIN, OUTPUT);
@@ -61,7 +64,10 @@ void turn_on_leds(unsigned char lumina){
 }
 
 void adc_start_conversion(void){
-  ADCSRA=0b11001000;
+  ADCSRA=0b10001000; //enable the ADC
+  for(long i=0;i<30000;i++);
+  ADCSRA=0b11001000; //incepem conversia
+  
 }
 
 unsigned char adc_get_result(void){
@@ -74,6 +80,7 @@ void main(void){
   
   while(1){
     adc_start_conversion();
+    for(long i=0;i<30000;i++); //delay
     unsigned char lumina=adc_get_result();
     
     turn_on_leds(lumina);
