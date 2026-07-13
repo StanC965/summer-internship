bbddd020eab3076188ed25c838cd561f99c15f4b
nelutuinfinite <81085177+nelutuinfinite@users.noreply.github.com>
2026-07-13 12:31:20 +0300

    Task 332

--- week1/Goal 2/button.c
+++ week1/Goal 2/button.c
@@ -4,12 +4,27 @@
 __interrupt void buton_apasat(void)
 {
   if(PINC_PINC6==0){
- btn ^= 1;
+    btn0^=1;
+  }
+  if(PINC_PINC1==0){
+  btn1^=1;
+  PORTD_PORTD5^=1;
   }
 }
 
+__interrupt void Button3(void)
+{
+  if(PINA_PINA0==0){
+ btn2 ^= 1;PORTD_PORTD4^=1;
+  }
+  if(PINA_PINA1==0){
+    btn3 ^= 1;PORTA_PORTA3^=1;
+  }
+}
+
+
 void PressToSos(){
-  if(btn==1){
+  if(btn0==1){
      Point(&PORTC,PIN7);
      Point(&PORTC,PIN7);
      Point(&PORTC,PIN7);
@@ -20,49 +35,49 @@ void PressToSos(){
      Point(&PORTC,PIN7);
      Point(&PORTC,PIN7);
      delay(5);
-     btn=0;
+     btn0=0;
   }
   }
 void SosStop(){
-   if(btn==1){
+   if(btn0==1){
      delay(2);
      while(1){
-       if(btn==0){
+       if(btn0==0){
          break;}
      Point(&PORTC,PIN7);
-     if(btn==0){
+     if(btn0==0){
        
        break;}
      Point(&PORTC,PIN7);
-     if(btn==0){
+     if(btn0==0){
          
          break;}
      Point(&PORTC,PIN7);
-    if(btn==0){
+    if(btn0==0){
          
          break;}
      Line(&PORTC,PIN7);
-     if(btn==0){
+     if(btn0==0){
         
          break;}
      Line(&PORTC,PIN7);
-     if(btn==0)
+     if(btn0==0)
          break;
      Line(&PORTC,PIN7);
-     if(btn==0){
+     if(btn0==0){
          
          break;}
      Point(&PORTC,PIN7);
-     if(btn==0){
+     if(btn0==0){
          
          break;}
      Point(&PORTC,PIN7);
-    if(btn==0){
+    if(btn0==0){
          
          break;}
      Point(&PORTC,PIN7);
      
      }
-     btn=0;
+     btn0=0;
    }
 }
--- week1/Goal 2/button.h
+++ week1/Goal 2/button.h
@@ -1,8 +1,14 @@
 #include "sos.h"
 #include "iom324pb.h"
 
-static volatile int btn=0;
+static unsigned volatile int btn0=0;
+static unsigned volatile int btn1=0;
+static unsigned volatile int btn2=0;
+static unsigned volatile int btn3=0;
 #pragma vector=PCINT2_vect
 __interrupt void buton_apasat(void);
+#pragma vector=PCINT0_vect
+__interrupt void Button3(void);
 void PressToSos();
 void SosStop();
+void ButtonPlay();
