744323e807424f9b333ffec8062da7f6739fab31
Raluca Fracea <ralucafracea16@gmail.com>
2026-07-13 11:00:42 +0300

    364

--- Week 2/Goal 3/main.c
+++ Week 2/Goal 3/main.c
@@ -9,23 +9,25 @@
 #define LED2_PIN 4      //pd4
 #define LED3_PIN 3      //pa3//pc7
 #define OUTPUT 1
-#define FULL_DARK 255
-#define SEMI_DARK 225
-#define SEMI_LIGHT 120
-#define FULL_LIGHT 60
+#define FULL_DARK 1023
+#define SEMI_DARK 800
+#define SEMI_LIGHT 440
+#define FULL_LIGHT 150
 
-volatile unsigned char lumina_ambientala=0;
+volatile unsigned int lumina_ambientala=0;
 
 #pragma vector=ADC_vect
 __interrupt void ADC_interrupt(void){
-  lumina_ambientala=ADCH;
-  ADCSRA = 0b00001000; //disable the ADC
+  unsigned char lumina_low=ADCL;
+  unsigned char lumina_high=ADCH;
+  lumina_ambientala=(lumina_high<<8) | lumina_low;
+  ADCSRA = 0b00001111; //disable the ADC
 }
 
 void adc_init(){
-  ADMUX=0b01100100; //REFS[1:0]-AVcc with external capacitor at AREF pin, ADLAR=1 - left adjust, MUX[4:0] - ADC4=00100
-  ADCSRA=0b00001000;  //ADEN=0 -disables the ADC, ADSC=0 - not starting the conversion, ADATE- no internal trigger signals, 
-                  //ADIF=0 -no interrupt flag, ADIE=1 -use interrupts ,ADPS[2:0]=000 - no prescale
+  ADMUX=0b01000100; //REFS[1:0]-AVcc with external capacitor at AREF pin, ADLAR=0 - RIGHT adjust, MUX[4:0] - ADC4=00100
+  ADCSRA=0b00001111;  //ADEN=0 -disables the ADC, ADSC=0 - not starting the conversion, ADATE- no internal trigger signals, 
+                  //ADIF=0 -no interrupt flag, ADIE=1 -use interrupts ,ADPS[2:0]=111 - prescale cu 128
   DIDR0=0b00010000; // disable ADC4D
   __enable_interrupt();
 }
@@ -40,7 +42,7 @@ void oled1_leds_init(){
   set_pin(&PORTA, LED3_PIN);
 }
 
-void turn_on_leds(unsigned char lumina){
+void turn_on_leds(unsigned int lumina){
   if(lumina>=SEMI_DARK && lumina <=FULL_DARK){
     set_pin(&PORTD, LED1_PIN); 
     set_pin(&PORTD, LED2_PIN); 
@@ -64,13 +66,13 @@ void turn_on_leds(unsigned char lumina){
 }
 
 void adc_start_conversion(void){
-  ADCSRA=0b10001000; //enable the ADC
+  ADCSRA=0b10001111; //enable the ADC
   for(long i=0;i<30000;i++);
-  ADCSRA=0b11001000; //incepem conversia
+  ADCSRA=0b11001111; //incepem conversia
   
 }
 
-unsigned char adc_get_result(void){
+unsigned int adc_get_result(void){
   return lumina_ambientala;
 }
 
@@ -81,7 +83,7 @@ void main(void){
   while(1){
     adc_start_conversion();
     for(long i=0;i<30000;i++); //delay
-    unsigned char lumina=adc_get_result();
+    unsigned int lumina=adc_get_result();
     
     turn_on_leds(lumina);
   }
