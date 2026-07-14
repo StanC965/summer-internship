1cbc68b5801201cc2ae2899775cc666007ca2e42
Cristina <cristina.saman@yahoo.ro>
2026-07-14 22:59:55 +0300

    refactor led module

--- code/services/led/led.c
+++ code/services/led/led.c
@@ -4,6 +4,7 @@
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Includes
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
 #include "led.h"
 #include <iom324pb.h>
 #include "gpio.h"
@@ -12,56 +13,62 @@
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Static private objects
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-typedef struct {
-    volatile uint8_t *ddr_register; 
-    volatile uint8_t *port_register; 
-    uint8_t pin;                     
-} led_config_t;
 
 static const led_config_t led_table[LED_COUNT] = {
     // ATmega328P onboard LED
-    { &DDRC, &PORTC, 7 },
+    {&DDRC, &PORTC, 7},
 
     // OLED1 LEDs
-    { &DDRD, &PORTD, 5 },
-    { &DDRD, &PORTD, 4 },
-    { &DDRA, &PORTA, 3 },
+    {&DDRD, &PORTD, 5},
+    {&DDRD, &PORTD, 4},
+    {&DDRA, &PORTA, 3},
 
     // IO1 LED
-    { &DDRB, &PORTB, 3 }
-};
+    {&DDRB, &PORTB, 3}};
 
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Implementation
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-void led_init(void){
-    for(uint8_t i = 0; i < LED_COUNT; i++){
+
+void led_init(void)
+{
+    for (uint8_t i = 0; i < LED_COUNT; i++)
+    {
         gpio_set_pin(led_table[i].port_register, led_table[i].pin);
         gpio_set_direction(led_table[i].ddr_register, GPIO_OUTPUT, led_table[i].pin);
     }
 }
 
-void led_power_on(led_id_t led_id){
-    if(led_id < LED_COUNT){
+void led_power_on(led_id_t led_id)
+{
+    if (led_id < LED_COUNT)
+    {
         gpio_reset_pin(led_table[led_id].port_register, led_table[led_id].pin);
     }
 }
 
-void led_power_off(led_id_t led_id){
-    if(led_id < LED_COUNT){
+void led_power_off(led_id_t led_id)
+{
+    if (led_id < LED_COUNT)
+    {
         gpio_set_pin(led_table[led_id].port_register, led_table[led_id].pin);
     }
 }
 
-void led_toggle(led_id_t led_id){
-    if(led_id < LED_COUNT){
+void led_toggle(led_id_t led_id)
+{
+    if (led_id < LED_COUNT)
+    {
         gpio_toggle_pin(led_table[led_id].port_register, led_table[led_id].pin);
     }
 }
 
-void led_blink_fast(led_id_t led_id){
-    if(led_id < LED_COUNT){
-        for(uint8_t i = 0; i < 5; i++){
+void led_blink_fast(led_id_t led_id)
+{
+    if (led_id < LED_COUNT)
+    {
+        for (uint8_t i = 0; i < 5; i++)
+        {
             led_power_on(led_id);
             delay(SECOND / 12);
 
@@ -69,11 +76,14 @@ void led_blink_fast(led_id_t led_id){
             delay(SECOND / 12);
         }
     }
-}  
+}
 
-void led_blink_slow(led_id_t led_id){
-    if(led_id < LED_COUNT){
-        for(uint8_t i = 0; i < 2; i++){
+void led_blink_slow(led_id_t led_id)
+{
+    if (led_id < LED_COUNT)
+    {
+        for (uint8_t i = 0; i < 2; i++)
+        {
             led_power_on(led_id);
             delay(SECOND / 4);
 
@@ -81,11 +91,14 @@ void led_blink_slow(led_id_t led_id){
             delay(SECOND / 4);
         }
     }
-}  
+}
 
-void led_blink_custom(led_id_t led_id, uint8_t times, uint32_t on_time, uint32_t off_time){
-    if(led_id < LED_COUNT){
-        for(uint8_t i = 0; i < times; i++){
+void led_blink_custom(led_id_t led_id, uint8_t times, uint32_t on_time, uint32_t off_time)
+{
+    if (led_id < LED_COUNT)
+    {
+        for (uint8_t i = 0; i < times; i++)
+        {
             led_power_on(led_id);
             delay(on_time);
 
--- code/services/led/led.h
+++ code/services/led/led.h
@@ -4,11 +4,20 @@
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Includes
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
 #include <stdint.h>
 
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Exported types and values
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+/** \brief  LED configuration structure */
+typedef struct
+{
+    volatile uint8_t *ddr_register;
+    volatile uint8_t *port_register;
+    uint8_t pin;
+} led_config_t;
+
 /** \brief  LED identifier type */
 typedef enum {
     // ATmega328P onboard LED
@@ -28,14 +37,16 @@ typedef enum {
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Public functions
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
 /** \fn     void led_init(void)
 
     \brief      [ Initializes the LED module. ]
     \param[in]  [ None ]
     \param[out] [ None ]
     \return     [ None ]
-    \details    [ This function initializes the LED module by setting the pins in the port registers to high 
-                  and configuring the corresponding data direction registers as outputs. ]
+    \details    [ This function initializes the LED module by setting 
+                 the pins in the port registers and configuring
+                 the corresponding data direction registers as outputs. ]
 */
 extern void led_init(void);
 
@@ -45,6 +56,8 @@ extern void led_init(void);
     \param[in]  led_id [ the LED identifier ]
     \param[out] [ None ]
     \return     [ None ]
+    \details    [ This function powers on the LED by setting 
+                 the corresponding bit in the port register. ]
 */
 extern void led_power_on(led_id_t led_id);
 
@@ -54,6 +67,8 @@ extern void led_power_on(led_id_t led_id);
     \param[in]  led_id [ the LED identifier ]
     \param[out] [ None ]
     \return     [ None ]
+    \details    [ This function powers off the LED by clearing 
+                 the corresponding bit in the port register. ]
 */
 extern void led_power_off(led_id_t led_id);
 
@@ -63,6 +78,8 @@ extern void led_power_off(led_id_t led_id);
     \param[in]  led_id [ the LED identifier ]
     \param[out] [ None ]
     \return     [ None ]
+    \details    [ This function toggles the state of the LED by 
+                 flipping the corresponding bit in the port register. ]
 */
 extern void led_toggle(led_id_t led_id);
 
@@ -72,7 +89,8 @@ extern void led_toggle(led_id_t led_id);
     \param[in]  led_id [ the LED identifier ]
     \param[out] [ None ]
     \return     [ None ]
-    \details    [ This function turns the LED on and off repetitively with 6-ons and 6-offs per second. ]
+    \details    [ This function turns the LED on and off repetitively
+                 with 6-ons and 6-offs per second. ]
 */
 extern void led_blink_fast(led_id_t led_id);
 
@@ -82,7 +100,8 @@ extern void led_blink_fast(led_id_t led_id);
     \param[in]  led_id [ the LED identifier ]
     \param[out] [ None ]
     \return     [ None ]
-    \details    [ This function turns the LED on and off repetitively with 2-ons and 2-offs per second.]
+    \details    [ This function turns the LED on and off repetitively
+                 with 2-ons and 2-offs per second.]
 */
 extern void led_blink_slow(led_id_t led_id);
 
