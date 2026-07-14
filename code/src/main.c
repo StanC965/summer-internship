9484a3d839a5c008e27ad6e4d130bd90852048ae
Cristina <cristina.saman@yahoo.ro>
2026-07-14 18:33:09 +0300

    objective 6: implement polling for button press/release for task 263

--- code/src/main.c
+++ code/src/main.c
@@ -4,6 +4,8 @@
 #include "sos.h"
 #include "delay.h"
 
+volatile uint8_t sos_active = 0;
+
 void main (void){
 
   led_init();
@@ -13,20 +15,34 @@ void main (void){
   uint8_t button_pressed;
   
   while(1){
+ 
     button_pressed = button_read(BUTTON_ONBOARD);
     
     if(!button_pressed){
+      
       delay(10 * MILISECOND);
       
-      button_pressed = button_read(BUTTON_ONBOARD);
-      
-      if(!button_pressed){
-        while(1){
-          sos_play(LED_ONBOARD);
-          delay(SECOND);
-      
+      if(!button_read(BUTTON_ONBOARD)){
+        
+        sos_active = !sos_active;
+        
+        if(!sos_active){
+          led_power_off(LED_ONBOARD);
         }
+        
+        while(!button_read(BUTTON_ONBOARD)){
+          delay(10 * MILISECOND);
+        }
+      
       }
+      
     }
+    
+    if(sos_active){
+      sos_update(LED_ONBOARD);
   }
+  
+  delay(1 * MILISECOND);
+  
+ }
 }
