bbde98a46835b912af3e052a31c4d22acf32e016
Iacob-Mircea <iacobmircea21@gmail.com>
2026-07-14 11:08:12 +0300

    681

--- Week3/Goal6/main.c
+++ Week3/Goal6/main.c
@@ -1,19 +1,5 @@
-#include "scheduler.h"
-#include "led.h"
 
-#define PC7 7
-//avantajul principal pe care il vad eu in a folosii poiteri la functii este faptul ca fiecare stare are o functie proprie , iar astfel codul este modular
-
-
-#pragma vector = TIMER1_COMPA_vect
-__interrupt void myInterrupt(void){
-    schedulerFlasgsManagement();
-
-}
-
-void main( void )
+int main( void )
 {
-  ledInit(&DDRC,&PORTC,PC7);
-  SREG |= 1<<7;
-  scheduleTaskDispatcher();
+  return 0;
 }
