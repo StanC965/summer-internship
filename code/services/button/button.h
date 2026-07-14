8782724fc654ac10bd58f11531f2327a11121ede
Cristina <cristina.saman@yahoo.ro>
2026-07-14 22:52:24 +0300

    refactor button module

--- code/services/button/button.h
+++ code/services/button/button.h
@@ -9,8 +9,18 @@
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Exported types and values
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+/** \brief  Button configuration structure */
+typedef struct
+{
+    volatile uint8_t *ddr_register;
+    volatile uint8_t *port_register;
+    volatile uint8_t *pin_register;
+    uint8_t pin;
+} button_config_t;
+
 /** \brief  Button identifier type */
-typedef enum {
+typedef enum
+{
     // ATmega328P onboard button
     BUTTON_ONBOARD = 0,
 
@@ -25,12 +35,15 @@ typedef enum {
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Public functions
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
 /** \fn     void button_init(void)
 
     \brief      [ Initializes the button module. ]
     \param[in]  [ None ]
     \param[out] [ None ]
     \return     [ None ]
+    \details    [ This function configures the data direction registers
+                 for all buttons, setting them as inputs. ]
 */
 extern void button_init(void);
 
@@ -40,6 +53,9 @@ extern void button_init(void);
     \param[in]  button_id [ the button identifier ]
     \param[out] [ None ]
     \return     [ None ]
+    \details    [ This function enables the pull-up resistor for the 
+                 specified button by setting the corresponding bit in 
+                 the port register. ]
 */
 extern void button_enable_pullup(button_id_t button_id);
 
@@ -49,6 +65,8 @@ extern void button_enable_pullup(button_id_t button_id);
     \param[in]  button_id [ the button identifier ]
     \param[out] [ None ]
     \return     [ The state of the button (1 for pressed, 0 for not pressed) ]
+    \details    [ This function reads the state of the specified button
+                 by checking the corresponding bit in the pin register. ]
 */
 extern uint8_t button_read(button_id_t button_id);
 
