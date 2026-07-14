d5e0d4c9c14ee3616933ff3c81f72fae037f8ea7
Raluca Fracea <ralucafracea16@gmail.com>
2026-07-14 14:13:49 +0300

    441- refacere + logic analyzer

--- Week 2/Goal 4/main.c
+++ Week 2/Goal 4/main.c
@@ -2,64 +2,45 @@
 #include "iom324.h"
 #include "led.h"
 #include "gpio.h"
+#include "CarCrashDetection.h"
 #include <intrinsics.h>
 
 #define SW0_PIN 6 //pc6
-#define LED1_PIN 5      //pd5
-#define LED2_PIN 4      //pd4
-#define LED3_PIN 3      //pa3
+#define AIRBAG_LED0_PIN 7 //pc7
 #define OUTPUT 1
 #define INPUT 0
 
-void timer1_init(void){
-  TCCR1A=0x00; //normal mode
-  TCCR1B=0x00; //timer oprit
-  TCNT1=0; //contor=0
+void timer0_init(void){
+  TCCR0A=0b00000010; //COM0A[1:0]=01, COM0B[1:0]- normal mode operation, 0x00 , WGM0[1:0]=10 -toggle OC0A on compare match
+  OCR0A=87;// 700=(8*(OCR0A+1))/1M => OCR0A=88-1=87
+  TIMSK0=0b00000010; //Overflow Interrupt Enable
+  TCCR0B=0b00000000; //prescale 0 - timer oprit
+  __enable_interrupt();
 }
 
-void leds_and_button_init(void){
-  set_direction(&DDRC, SW0_PIN,INPUT);
-  set_pin(&PORTC,SW0_PIN);
-  
-  Init_LED(&DDRD,LED1_PIN,OUTPUT);
-  Init_LED(&DDRD,LED2_PIN,OUTPUT);
-  Init_LED(&DDRA,LED3_PIN,OUTPUT);
-  set_pin(&PORTD,LED1_PIN);
-  set_pin(&PORTD,LED2_PIN);
-  set_pin(&PORTA,LED3_PIN);
+void airbag_init(void){
+  Init_LED(&DDRC,AIRBAG_LED0_PIN,OUTPUT);
+  set_pin(&PORTC,AIRBAG_LED0_PIN);
+  set_direction(&DDRC, SW0_PIN, INPUT);
+  set_pin(&PORTC, SW0_PIN);
 }
 
-void main( void ){
-  leds_and_button_init();
-  timer1_init();
+
+#pragma vector=TIMER0_COMPA_vect
+__interrupt void timer0_compa_interrupt(void){
+  reset_pin(&PORTC, AIRBAG_LED0_PIN); //declansarea airbag-ului
+  TCCR0B=0b00000000; //oprim timer ul
+}
+void main( void )
+{
+  airbag_init();
+  timer0_init();
+  unsigned char airbag=0;
   while(1){
-    if(read_pin(&PINC, SW0_PIN)==0){
-      set_pin(&PORTC,LED1_PIN);
-      set_pin(&PORTC,LED2_PIN);
-      set_pin(&PORTA,LED3_PIN);
-      TCNT1=0;
-      TCCR1B=0b00000101; //prescale 1024
-      
-      while(read_pin(&PINC, SW0_PIN)==0); //cat timp e apasat
-      TCCR1B=0x00;//oprim timer ul
-      unsigned int timp=TCNT1; //citim cat timp a fost apasat butonul
-      
-      if(timp<977){
-        reset_pin(&PORTD,LED1_PIN);
-        set_pin(&PORTD,LED2_PIN);
-        set_pin(&PORTA,LED3_PIN);
-      }
-      else if(timp>=977 && timp<4885){
-        reset_pin(&PORTD,LED2_PIN);
-        set_pin(&PORTA,LED3_PIN);
-        set_pin(&PORTD,LED1_PIN);
-      }
-      else{
-        reset_pin(&PORTA,LED3_PIN);
-        set_pin(&PORTD,LED2_PIN);
-        set_pin(&PORTD,LED1_PIN);
-      }
-      
+    if(read_pin(&PINC, SW0_PIN)==0 && airbag==0){
+      airbag=1;
+      TCNT0=0; //incepe numaratoarea
+      TCCR0B=0b00000010;
     }
   }
 }
