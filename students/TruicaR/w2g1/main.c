31b3ef21ee5a665b5427e71f49b9e60ed660c56c
Truica Robert <robertioan.truica@ulbsibiu.ro>
2026-07-15 14:19:37 +0300

    w2g1-task355: extract app logic into panel module, keeping gpio/led/adc as generic drivers

--- students/TruicaR/w2g1/main.c
+++ students/TruicaR/w2g1/main.c
@@ -1,131 +1,13 @@
-#include "iom324pb.h"
-#include "gpio.h"
-#include "led.h"
-#include "sos.h"
+#include "panel.h"
 #include <intrinsics.h>
-#include "adc.h"
-
-#define GPIO_INPUT   0
-#define GPIO_OUTPUT  1
-#define PULLUP_ON    1
-#define INTERRUPT_ENABLED  1
-
-#define BUTTON1_PIN  1   
-#define BUTTON2_PIN  0  
-#define BUTTON3_PIN  1  
-#define LED1_PIN     5   
-#define LED2_PIN     4   
-#define LED3_PIN     3  
-#define LED0_PIN     7  
-#define LIGHT_SENSOR_ADC_CHANNEL  4
-
-static unsigned char led1_state = 0;
-static unsigned char led2_state = 0;
-static unsigned char led3_state = 0;
-
-static unsigned char button1_prev = 0;
-static unsigned char button2_prev = 0;
-static unsigned char button3_prev = 0;
-static volatile unsigned char panel_blocked = 0;
-
-#pragma vector = PCINT2_vect
-__interrupt void pcint2_isr(void)
-{
-    if (PINC_PINC6 == 0)   
-    {
-        panel_blocked = !panel_blocked;
-
-        if (panel_blocked)
-        {
-            led_off(&PORTD, LED1_PIN);
-            led_off(&PORTD, LED2_PIN);
-            led_off(&PORTA, LED3_PIN);
-            led1_state = 0;
-            led2_state = 0;
-            led3_state = 0;
-            led_on(&PORTC, LED0_PIN);
-        }
-        else
-        {
-            led_off(&PORTC, LED0_PIN);
-        }
-    }
-}
 
 void main(void)
 {
-
-    DDRC_DDRC6 = GPIO_INPUT;
-    PORTC_PORTC6 = PULLUP_ON;
-    PCMSK2_PCINT22 = INTERRUPT_ENABLED;
-    PCICR_PCIE2 = INTERRUPT_ENABLED;
-
-
-    gpio_set_direction(&DDRC, BUTTON1_PIN, GPIO_INPUT);
-    gpio_set_pin(&PORTC, BUTTON1_PIN);
-
-    gpio_set_direction(&DDRA, BUTTON2_PIN, GPIO_INPUT);
-    gpio_set_pin(&PORTA, BUTTON2_PIN);
-
-    gpio_set_direction(&DDRA, BUTTON3_PIN, GPIO_INPUT);
-    gpio_set_pin(&PORTA, BUTTON3_PIN);
-
-    gpio_set_direction(&DDRD, LED1_PIN, GPIO_OUTPUT);
-    gpio_set_direction(&DDRD, LED2_PIN, GPIO_OUTPUT);
-    gpio_set_direction(&DDRA, LED3_PIN, GPIO_OUTPUT);
-    gpio_set_direction(&DDRC, LED0_PIN, GPIO_OUTPUT);
-
-    led_off(&PORTD, LED1_PIN);
-    led_off(&PORTD, LED2_PIN);
-    led_off(&PORTA, LED3_PIN);
-    led_off(&PORTC, LED0_PIN);
-
+    panel_init();
     __enable_interrupt();
-    adc_init(LIGHT_SENSOR_ADC_CHANNEL);
+
     while (1)
     {
-    unsigned char b1_now = gpio_debounce(&PINC, BUTTON1_PIN);
-    unsigned char b2_now = gpio_debounce(&PINA, BUTTON2_PIN);
-    unsigned char b3_now = gpio_debounce(&PINA, BUTTON3_PIN);
-
-    unsigned char any_press = (b1_now && !button1_prev) ||
-                               (b2_now && !button2_prev) ||
-                               (b3_now && !button3_prev);
-
-    if (panel_blocked)
-    {
-        if (any_press)
-        {
-            
-            led_off(&PORTC, LED0_PIN);
-            for (volatile int i = 0; i < 15000; i++);
-            led_on(&PORTC, LED0_PIN);
-        }
-    }
-    else
-    {
-        if (b1_now && !button1_prev)
-        {
-            led1_state = !led1_state;
-            if (led1_state) led_on(&PORTD, LED1_PIN); else led_off(&PORTD, LED1_PIN);
-        }
-        if (b2_now && !button2_prev)
-        {
-            led2_state = !led2_state;
-            if (led2_state) led_on(&PORTD, LED2_PIN); else led_off(&PORTD, LED2_PIN);
-        }
-        if (b3_now && !button3_prev)
-        {
-            led3_state = !led3_state;
-            if (led3_state) led_on(&PORTA, LED3_PIN); else led_off(&PORTA, LED3_PIN);
-        }
-    }
-
-    button1_prev = b1_now;
-    button2_prev = b2_now;
-    button3_prev = b3_now;
-    
-    adc_start_conversion();
-    for (volatile long i = 0; i < 50000; i++); 
+        panel_update();
     }
 }
