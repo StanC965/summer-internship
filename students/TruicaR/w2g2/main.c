af074cca3b126455194b75123b70c7c194e9b0e9
Truica Robert <robertioan.truica@ulbsibiu.ro>
2026-07-17 15:25:38 +0300

    w2g2-task441 - integrate CarCrashDetection module, airbag LED triggered at 768us via single status check + CTC match counting

--- students/TruicaR/w2g2/main.c
+++ students/TruicaR/w2g2/main.c
@@ -1,33 +1,49 @@
 #include "iom324pb.h"
-#include "tc0.h"
 #include "gpio.h"
 #include "led.h"
+#include "CarCrashDetection.h"
 #include <intrinsics.h>
 
-#define LED0_PIN               7
-#define TICKS_FOR_400MS_PERIOD 200   
+#define AIRBAG_LED_PIN       5   
+#define MATCHES_TO_TRIGGER   6   
 
-static volatile unsigned int tick_count = 0;
+static volatile unsigned char match_count = 0;
 
 #pragma vector = TIMER0_COMPA_vect
 __interrupt void tc0_compa_isr(void)
 {
-    tick_count++;
-    if (tick_count >= TICKS_FOR_400MS_PERIOD)
+    match_count++;
+    if (match_count >= MATCHES_TO_TRIGGER)
     {
-        tick_count = 0;
-        gpio_read_pin(&PINC, LED0_PIN) ? led_off(&PORTC, LED0_PIN) : led_on(&PORTC, LED0_PIN);
+        led_on(&PORTD, AIRBAG_LED_PIN);  
+        TCCR0B_CS00 = 0;                 
+        TIMSK0_OCIE0A = 0;
     }
 }
 
 void main(void)
 {
-    gpio_set_direction(&DDRC, LED0_PIN, 1);
-    led_off(&PORTC, LED0_PIN);
+    unsigned char status;
 
-    tc0_ctc_init_interrupt_only(124);   
+    gpio_set_direction(&DDRD, AIRBAG_LED_PIN, 1);
+    led_off(&PORTD, AIRBAG_LED_PIN);
+
+    TCCR0A_WGM01 = 1;
+    TCCR0A_WGM00 = 0;
+    TCCR0B_WGM02 = 0;
+    OCR0A = 127;
+    TIMSK0_OCIE0A = 1;
 
     __enable_interrupt();
+
+    status = GetCarCrashDetectionStatus();
+
+    if (status == CRASH)
+    {
+        match_count = 0;
+        TCNT0 = 0;
+        TCCR0B_CS00 = 1;  
+    }
 
     while (1) {}
 }
