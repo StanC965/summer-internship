f201474fa2e0e3a7e5c2da14c62b9c86a558ab03
unknown <stefanadrian.deac@ulbsibiu.ro>
2026-07-17 13:30:58 +0300

    EX 512 feat: added timer modules

--- Exercitii/main.c
+++ Exercitii/main.c
@@ -1,3 +1,7 @@
+#include "iom324pb.h"
+#include "gpio.h"
+#include "led.h"
+#include "timer.h"
 
 /**
  * @brief Adds two numbers.
@@ -8,7 +12,35 @@
  */
 
 
+void setup()
+{
+  
+    leds_initialize(1,0,0,0,0);
+    led_Set(LED_ZERO);
+    
+   
+
+    
+}
+
 int main( void )
 {
-  return 0;
+setup();
+
+    
+    if (Timer1_ales_exact_8Mhz(10) ==1) {
+        
+    } else {
+     
+        Timer1_ales_aprox_8Mhz(10);
+    }
+
+    while(1)
+    {
+      
+      
+      
+      
+    }
+    return 0;
 }
