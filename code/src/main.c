bc9f9b4fc3de00cf8bb923ea82e3dbae216944ac
Cristina <cristina.saman@yahoo.ro>
2026-07-19 00:16:07 +0300

    feat: implement 4 reading intervals for light sensor

--- code/src/main.c
+++ code/src/main.c
@@ -8,7 +8,10 @@
 
 extern volatile uint8_t light_sensor_value;
 
-#define LIGHT_SENSOR_MIDPOINT (127U)
+#define LIGHT_SENSOR_FULL_DARK    (255U)
+#define LIGHT_SENSOR_SEMI_DARK    (170U)
+#define LIGHT_SENSOR_SEMI_LIGHT   (85U)
+#define LIGHT_SENSOR_FULL_LIGHT   (0U)
 
 void main(void)
 {
@@ -20,13 +23,26 @@ void main(void)
   while (1)
   {
     adc_start_conversion();
-    if (light_sensor_value < LIGHT_SENSOR_MIDPOINT)
+    if (light_sensor_value < LIGHT_SENSOR_SEMI_LIGHT)
     {
-      led_power_on(LED_ONBOARD);
+      led_power_on(LED_OLED1_1);
+      led_power_on(LED_OLED1_2);
+      led_power_on(LED_OLED1_3);
     }
-    else
+    else if (light_sensor_value >= LIGHT_SENSOR_SEMI_LIGHT && light_sensor_value < LIGHT_SENSOR_SEMI_DARK)
     {
-      led_power_off(LED_ONBOARD);
+      led_power_on(LED_OLED1_1);
+      led_power_on(LED_OLED1_2);
+    }
+     else if (light_sensor_value >= LIGHT_SENSOR_SEMI_DARK && light_sensor_value < LIGHT_SENSOR_FULL_DARK)
+    {
+      led_power_on(LED_OLED1_1);
+    }
+    else 
+    {
+      led_power_off(LED_OLED1_1);
+      led_power_off(LED_OLED1_2);
+      led_power_off(LED_OLED1_3);
     }
     // delay(1 * MILISECOND);
   }
