f87955564a8a40644baa3996aa740878ae478fb3
Cristina <cristina.saman@yahoo.ro>
2026-07-19 22:17:14 +0300

    feat: implement the timer module

---
+++ code/drivers/timer/timer.c
@@ -0,0 +1,53 @@
+#ifndef TIMER_C
+#define TIMER_C
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Includes
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+#include "timer.h"
+#include <iom324pb.h>
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Static private objects & functions
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Implementation
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+void timer_init(void)
+{
+    timer_enable_peripheral_clock();
+    timer_select_normal_mode();
+    timer_configure_control_settings();
+}
+
+void timer_enable_peripheral_clock(void)
+{
+    PRR0 &= ~BIT_MASK(PRTIM0);
+}
+
+void timer_select_normal_mode(void)
+{
+    TCCR0A = 0x00;
+}
+
+void timer_configure_control_settings(void)
+{
+    TCCR0B &= ~(TCCR0B_CS_MASK);
+    timer_enable_overflow_interrupt();
+}
+
+void timer_enable_overflow_interrupt()
+{
+    TIMSK0 |= (1 << TOIE0);
+}
+
+void timer_start_no_prescaling(void)
+{
+    TCCR0B &= ~TCCR0B_CS_MASK;
+    TCCR0B |= (TIMER_PRESCALER_NO_DIVISION & TCCR0B_CS_MASK);
+}
+
+#endif /* TIMER_C */
---
+++ code/drivers/timer/timer.h
@@ -0,0 +1,73 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Includes
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+#include <stdint.h>
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Exported types and values
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+/** \brief  Macro utility to generate a bit mask for a specific bit position */
+#define BIT_MASK(bit_position)        (1 << bit_position)
+
+#define TCCR0B_CS_MASK                (0x07U)
+#define TIMER_PRESCALER_NO_DIVISION   (0X01U)
+#define PRTIM0                        5
+#define TOIE0                         0
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Public functions
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+/** \fn      void timer_init(void)
+    \brief   [ Initializes the ADC module. ]
+    \param   [ None ] 
+    \return  [ None ]
+    \details [ Performs all necessary initialization steps for the ADC module,
+             configuring initial references and control registers. ]
+*/
+extern void timer_init(void);
+
+/** \fn      void timer_enable_peripheral_clock(void)
+    \brief   [ Initializes the ADC module. ]
+    \param   [ None ] 
+    \return  [ None ]
+    \details [ Performs all necessary initialization steps for the ADC module,
+             configuring initial references and control registers. ]
+*/
+extern void timer_enable_peripheral_clock(void);
+
+/** \fn      void timer_select_normal_mode(void)
+    \brief   [ Initializes the ADC module. ]
+    \param   [ None ] 
+    \return  [ None ]
+    \details [ Performs all necessary initialization steps for the ADC module,
+             configuring initial references and control registers. ]
+*/
+extern void timer_select_normal_mode(void);
+
+/** \fn      void timer_configure_control_settings(void)
+    \brief   [ Initializes the ADC module. ]
+    \param   [ None ] 
+    \return  [ None ]
+    \details [ Performs all necessary initialization steps for the ADC module,
+             configuring initial references and control registers. ]
+*/
+extern void timer_configure_control_settings(void);
+
+/** \fn      void timer_enable_overflow_interrupt()
+    \brief   [ Initializes the ADC module. ]
+    \param   [ None ] 
+    \return  [ None ]
+    \details [ Performs all necessary initialization steps for the ADC module,
+             configuring initial references and control registers. ]
+*/
+extern void timer_enable_overflow_interrupt(void);
+
+extern void timer_start_no_prescaling(void);
+
+#endif /* TIMER_H */
+
