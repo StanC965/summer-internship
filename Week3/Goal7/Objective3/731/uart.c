8fbde20ab09c09c69103dc2869ffcaa06290bc4c
Sebi0411 <sebi041102@gmail.com>
2026-07-20 14:11:58 +0300

    731 feat(uart):  UART driver for EXT4 and basic string communication

---
+++ Week3/Goal7/Objective3/731/uart.c
@@ -0,0 +1,59 @@
+#ifndef UART_C
+#define UART_C
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Includes
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+#include "uart.h"
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+/* Implementation                                                            */
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+void uart_init(void)
+{
+    unsigned int ubrr_value = (unsigned int)((UART_SYSTEM_CLOCK_FREQ / (16UL * UART_DEFAULT_BAUD)) - 1);
+
+    // Baud rate high and low registers
+    UBRR2H = (unsigned char)(ubrr_value >> 8);
+    UBRR2L = (unsigned char)(ubrr_value);
+
+    // Enable Receiver and Transmitter
+    UCSR2B = (1 << 4) | (1 << 3);
+
+    
+    // Select 8-bit character data size.
+    UCSR2C = (1 << 2) | (1 << 1);
+}
+
+unsigned char uart_is_tx_ready(void)
+{
+    return (UCSR2A & (1 << 5)) ? 1 : 0;
+}
+
+void uart_send_byte(unsigned char data)
+{
+    UDR2 = data;
+}
+
+unsigned char uart_is_rx_available(void)
+{
+    return (UCSR2A & (1 << 7)) ? 1 : 0;
+}
+
+unsigned char uart_read_byte(void)
+{
+    return UDR2;
+}
+
+void uart_send_string(const char *str)
+{
+    while (*str != '\0')
+    {
+        while (!uart_is_tx_ready());
+        uart_send_byte((unsigned char)(*str));
+        str++;
+    }
+}
+
+#endif
