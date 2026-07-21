f8724c1dab69ef95a99fd0b83ea96b51dcec26fc
Raluca Fracea <ralucafracea16@gmail.com>
2026-07-21 18:59:57 +0300

    681

--- Week 3/Goal 6/scheduler_cfg.c
+++ Week 3/Goal 6/scheduler_cfg.c
@@ -3,57 +3,144 @@
 #include "adc.h"
 #include "led.h"
 #include "gpio.h"
-#include "pwm.h"
 
 #define BTNSW0_PIN 6    //pc6
+#define BTN1_PIN 1      //pc1
+#define BTN2_PIN 0      //pa0
+#define BTN3_PIN 1      //pa1
 #define LED0_PIN 7      //pc7
 #define LED1_PIN 5      //pd5
 #define LED2_PIN 4      //pd4
-//#define PWM_PIN 3 //pb3
+#define LED3_PIN 3      //pa3
 #define OUTPUT 1
 #define INPUT 0
-#define NIGHT_MODE 0
-#define DAY_MODE 1
-#define STATE_IDLE 0
-#define STATE_START 1
-
-typedef enum {
-  STATE_OFF,
-  STATE_ON
-} blinking;
 
 volatile unsigned char btn0=1;
+volatile unsigned char btn1=1;
+volatile unsigned char btn2=1;
+volatile unsigned char btn3=1;
 
 void task_10ms(void) {
-  static blinking state=STATE_OFF;
-  static int timer=0;
-  timer++;
+  static unsigned char buffer0=0xFF;
+  static unsigned char buffer1=0xFF;
+  static unsigned char buffer2=0xFF;
+  static unsigned char buffer3=0xFF;
   
-  switch(state){
-  case STATE_OFF:
-    if(timer>=100){
-      timer=0;
-      set_pin(&PORTC,LED0_PIN);
-      state=STATE_ON;
-    }
-    break;
-    
-  case STATE_ON:
-    if(timer>=100){
-      timer=0;
-      reset_pin(&PORTC,LED0_PIN);
-      state=STATE_OFF;
-    }
-    break;
-    
-  default:
-    state=STATE_OFF;
-    break;
-  }
+  unsigned char stare_btn0=(PINC&(1<<BTNSW0_PIN))?1:0;
+  unsigned char stare_btn1=(PINC&(1<<BTN1_PIN))?1:0;
+  unsigned char stare_btn2=(PINA&(1<<BTN2_PIN))?1:0;
+  unsigned char stare_btn3=(PINA&(1<<BTN3_PIN))?1:0;
+  
+  buffer0=(buffer0<<1)|stare_btn0;
+  buffer1=(buffer1<<1)|stare_btn1;
+  buffer2=(buffer2<<1)|stare_btn2;
+  buffer3=(buffer3<<1)|stare_btn3;
+  
+  unsigned char window0=buffer0&0x1F; //00011111
+  unsigned char window1=buffer1&0x1F;
+  unsigned char window2=buffer2&0x1F;
+  unsigned char window3=buffer3&0x1F;
+  
+  if(window0 == 0x00) 
+    btn0 = 0; 
+  else if(window0 == 0x1F) 
+    btn0 = 1;
+  if(window1 == 0x00) 
+    btn1 = 0; 
+  else if(window1 == 0x1F) 
+    btn1 = 1;
+  if(window2 == 0x00) 
+    btn2 = 0; 
+  else if(window2 == 0x1F) 
+    btn2 = 1;
+  if(window3 == 0x00) 
+    btn3 = 0; 
+  else if(window3 == 0x1F) 
+    btn3 = 1;
+  
   
 }
 
+typedef enum {
+    STATE_UNBLOCKED,
+    STATE_BLOCKED
+} panel_state;
+
 void task_50ms(void) {
+  static panel_state state=STATE_UNBLOCKED;
+  static int stare_led1 = 0;
+  static int stare_led2 = 0;
+  static int stare_led3 = 0;
+  static int stare_btn0=1;
+  static int stare_btn1=1;
+  static int stare_btn2=1;
+  static int stare_btn3=1;
+  
+  switch(state){
+  case STATE_UNBLOCKED:
+    if(btn0==0 && stare_btn0==1){
+    reset_pin(&PORTC,LED0_PIN);
+    set_pin(&PORTD,LED1_PIN);
+    set_pin(&PORTD,LED2_PIN);
+    set_pin(&PORTA,LED3_PIN);
+    stare_led1=0;
+    stare_led2=0;
+    stare_led3=0;   
+    state=STATE_BLOCKED;
+    }
+    if(btn1==0 && stare_btn1==1){
+      if(stare_led1==0){
+      reset_pin(&PORTD,LED1_PIN);
+      stare_led1=1;
+    } 
+    else{
+      set_pin(&PORTD,LED1_PIN);
+      stare_led1=0;
+    }
+    }
+    
+    if(btn2==0 && stare_btn2==1){
+      if(stare_led2==0){
+      reset_pin(&PORTD,LED2_PIN);
+      stare_led2=1;
+    }
+  
+    else{
+      set_pin(&PORTD,LED2_PIN);
+      stare_led2=0;
+    }
+    }
+    
+    if(btn3==0 && stare_btn3==1){
+       if(stare_led3==0){
+      reset_pin(&PORTA,LED3_PIN);
+      stare_led3=1;
+    }
+  
+    else{
+      set_pin(&PORTA,LED3_PIN);
+      stare_led3=0;
+    }
+    }
+    break;
+    
+    
+  case STATE_BLOCKED:
+     if(btn0==0 && stare_btn0==1){
+       set_pin(&PORTC,LED0_PIN);
+       state=STATE_UNBLOCKED;
+     }
+     if((btn1==0 && stare_btn1==1)||(btn2==0 && stare_btn2==1)||(btn3==0 && stare_btn3==1)){
+       BlinkFast_LED(&PORTC, LED0_PIN);
+       reset_pin(&PORTC,LED0_PIN);
+     }
+    break;
+    
+  }
+  stare_btn0=btn0;  
+  stare_btn1=btn1;    
+  stare_btn2=btn2;
+  stare_btn3=btn3;
   
 }
 
