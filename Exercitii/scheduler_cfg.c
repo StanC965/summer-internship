5d11cd6c22bca561aa0c0a09896c35a48493e6f1
unknown <stefanadrian.deac@ulbsibiu.ro>
2026-07-21 09:35:36 +0300

    ex 634   feat : added the hearbeat illumination signal based on the values provided inside the excel

--- Exercitii/scheduler_cfg.c
+++ Exercitii/scheduler_cfg.c
@@ -8,40 +8,60 @@
 #include "led.h"
 
 
-volatile short sequence[5] = {0, 64, 128, 192, 255};
+volatile short sequence[33] = {0,0,0,1,1,1,1,2,2,21,41,60,80,57,33,10,33,55,77,100,92,85,77,69,62,54,46,38,31,23,15,8,0};
 volatile unsigned char index = 0;
-volatile int completion_index = 0; 
 
-volatile _Bool increasing=1;
+
+
 
 void task_10ms(void)
 {
    
 }
+/*
+0	0.00
+0.05	0.22
+0.1	0.44
+0.15	0.65
+0.2	0.87
+0.25	1.09
+0.3	1.31
+0.35	1.52
+0.4	1.74
+0.45	21.31
+0.5	40.87
+0.55	60.44         HEARTBEAT
+0.6	80.00
+0.65	56.67
+0.7	33.33
+0.75	10.00
+0.8	32.50
+0.85	55.00
+0.9	77.50
+0.95	100.00
+1	92.31
+1.05	84.62
+1.1	76.92
+1.15	69.23
+1.2	61.54
+1.25	53.85
+1.3	46.15
+1.35	38.46
+1.4	30.77
+1.45	23.08
+1.5	15.38
+1.55	7.69
+1.6	0.00
+*/
 
 void task_50ms(void)
 {
    
-
-    int true__value=0;
-      completion_index +=2;     
-    if(increasing)
-    {
-
-   true__value=100-completion_index;    
-    }
-    else{
-      true__value=completion_index;
-    }
-  pwm_dc(true__value);
-   if(completion_index ==100)
-   {
-      completion_index=0;
-      
-     increasing=!increasing;        //cea mai importanta parte in acest semnal sinusoidal
-  
-     
-   }
+  int true_value=100-sequence[index];
+ pwm_dc(true_value);
+ index++;
+ if (index==33)
+   index=0;
    
   
   
