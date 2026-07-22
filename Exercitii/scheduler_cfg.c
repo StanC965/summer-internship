b4e3a9aa747bdf01da957af627ecae6a54de4df3
unknown <stefanadrian.deac@ulbsibiu.ro>
2026-07-22 13:52:30 +0300

    ex 731 feat : transmit a whole sentence at a faster rate

--- Exercitii/scheduler_cfg.c
+++ Exercitii/scheduler_cfg.c
@@ -10,10 +10,16 @@
 #include "usart.h"
 
 
-
+volatile char sentence[20]={"SALUTARE_sii_ce_faci"};
+volatile unsigned char i=0;
 void task_10ms(void)
 {
- 
+   unsigned char temp= sentence[i];
+
+  USART_Transmit( temp);
+  i++;
+  if(i==21)
+  i=0;    
 }
 
 void task_50ms(void)
@@ -24,7 +30,10 @@ void task_50ms(void)
 
 void task_100ms(void) {}
 void task_500ms(void) {}
-void task_1000ms(void) {
-  USART_Transmit( 89);}
+void task_1000ms(void)
+{
+
+
+}
 
 #endif
--- Exercitii/usart.c
+++ Exercitii/usart.c
@@ -19,22 +19,22 @@
 
   void USART_Init( unsigned int ubrr)
 {
-/*Set baud rate */
+
 UBRR2H = (unsigned char)(ubrr>>8);
 UBRR2L = (unsigned char)ubrr;
-/*Enable receiver and transmitter */
+
 UCSR2B = (1<<4)|(1<<3);
-/* Set frame format: 8data, 2stop bit */
+
 UCSR2C = (1<<3)|(1<<1)|(1<<2);
   
 }
 
 void USART_Transmit( unsigned char data )
 {
-/* Wait for empty transmit buffer */
+
 while ( !( UCSR2A & (1<<5)) )
 ;
-/* Put data into buffer, sends the data */
+
 UDR2 = data;
 }
 
