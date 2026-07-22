b5ef7e587b538d42dfa6493075cb0e9e08757f7a
Cristina <cristina.saman@yahoo.ro>
2026-07-22 12:30:56 +0300

    style(lighting): format module

--- code/app/lighting/lighting.c
+++ code/app/lighting/lighting.c
@@ -6,20 +6,14 @@
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
 #include "lighting.h"
-#include "adc.h"
 #include <iom324pb.h>
+#include "adc.h"
 #include "timer.h"
+#include "bits.h"
 
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    Exported types and values
+    Static private objects & functions
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-typedef struct
-{
-    uint8_t perceived_min;
-    uint8_t perceived_max;
-    uint8_t absolute_entry_percent;
-    light_mode_t mode;
-} light_band_t;
 
 static const light_band_t light_bands[] = {
     {0, 20, 15, LIGHT_MODE_NIGHT},
@@ -31,37 +25,23 @@ static const light_band_t light_bands[] = {
 
 #define LIGHT_BAND_COUNT (sizeof(light_bands) / sizeof(light_bands[0]))
 
-#define ENTRY_PHASE_STEP_COUNT (11U)
-
 static const int8_t night_entry_steps[ENTRY_PHASE_STEP_COUNT] =
     {0, 1, 1, 3, 7, 16, 16, 14, 12, 10, 8};
 
 static const int8_t day_entry_steps[ENTRY_PHASE_STEP_COUNT] =
     {0, 4, 7, 16, 44, 59, 59, 55, 50, 45, 40};
 
-#define LIGHTING_PWM_TOP (1249U)
-
 static void lighting_set_duty_percent(uint8_t duty_percent)
 {
     uint16_t compare_value = ((uint32_t)duty_percent * LIGHTING_PWM_TOP) / 100U;
-    OCR1A = compare_value; 
-    OCR1B = compare_value; 
+    OCR1A = compare_value;
+    OCR1B = compare_value;
 }
-
-typedef enum
-{
-    ENTRY_PHASE_IDLE = 0,
-    ENTRY_PHASE_RUNNING
-} entry_phase_state_t;
 
 static entry_phase_state_t entry_state = ENTRY_PHASE_IDLE;
 static uint8_t entry_step_index = 0;
 static int16_t current_duty = 0;
 static light_mode_t current_mode = LIGHT_MODE_NIGHT;
-
-/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    Implementation
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
 static uint8_t adc_to_perceived_light(uint16_t adc_raw)
 {
@@ -78,25 +58,28 @@ static const light_band_t *lighting_find_band(uint8_t perceived_level)
             return &light_bands[i];
         }
     }
-    return &light_bands[0]; 
+    return &light_bands[0];
 }
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Implementation
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
 void lighting_init(void)
 {
-
-    TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11);
-    TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11); 
+    TCCR1A = BIT_MASK(COM1A1) | BIT_MASK(COM1B1) | BIT_MASK(WGM11);
+    TCCR1B = BIT_MASK(WGM13) | BIT_MASK(WGM12) | BIT_MASK(CS11);
     ICR1 = LIGHTING_PWM_TOP;
 
     lighting_set_duty_percent(0);
 
-    TIMSK1 |= (1 << TOIE1); 
+    TIMSK1 |= BIT_MASK(TOIE1);
 }
 
 void lighting_trigger_entry_phase(void)
 {
     adc_start_conversion();
-    while (ADCSRA & (1 << ADSC))
+    while (ADCSRA & (BIT_MASK(ADSC)))
     {
     }
 
--- code/app/lighting/lighting.h
+++ code/app/lighting/lighting.h
@@ -10,11 +10,32 @@
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Exported types and values
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+typedef struct
+{
+    uint8_t perceived_min;
+    uint8_t perceived_max;
+    uint8_t absolute_entry_percent;
+    light_mode_t mode;
+} light_band_t;
+
+
 typedef enum
 {
     LIGHT_MODE_NIGHT = 0,
     LIGHT_MODE_DAY
 } light_mode_t;
+
+#define ENTRY_PHASE_STEP_COUNT (11U)
+
+
+#define LIGHTING_PWM_TOP (1249U)
+
+
+typedef enum
+{
+    ENTRY_PHASE_IDLE = 0,
+    ENTRY_PHASE_RUNNING
+} entry_phase_state_t;
 
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Public functions
@@ -25,12 +46,24 @@ typedef enum
     \param   [ None ]
     \return  [ None ]
     \details [ Performs all necessary initialization steps for the ADC module,
-             configuring initial references and control registers. ]
+               configuring initial references and control registers. ]
 */
 extern void lighting_init(void);
 
+/** \fn      void lighting_trigger_entry_phase(void)
+    \brief   [ ]
+    \param   [ None ]
+    \return  [ None ]
+    \details [ ]
+*/
 extern void lighting_trigger_entry_phase(void);
 
+/** \fn      void lighting_entry_phase_update(void
+    \brief   [ ]
+    \param   [ None ]
+    \return  [ None ]
+    \details [ ]
+*/
 extern void lighting_entry_phase_update(void);
 
 #endif /* LIGHTNG_H */
