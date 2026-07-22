3b602209ffe0668e9c73825c268d3ddfd1c9a823
Raluca Fracea <ralucafracea16@gmail.com>
2026-07-22 13:44:11 +0300

    731

---
+++ Week 3/Goal 7/main.c
@@ -0,0 +1,14 @@
+#include "iom324pb.h"
+#include "uart.h"
+
+void main( void )
+{
+  uart_init(103); //pentru 9600 baud
+  
+  unsigned char caracter;
+  while(1){
+    uart_transmit_char('R');
+    caracter=uart_receive();
+  }
+  
+}
