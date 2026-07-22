908609e9ca816e629a832e1a63ccfe54f646d13a
bianca.cotofana <biancamaria.cotofana@ulbsibiu.ro>
2026-07-22 05:25:08 +0300

    425 - LED0 blinks 1s using TC0 normal mode

--- Week2Goal4/main.c
+++ Week2Goal4/main.c
@@ -4,12 +4,20 @@
 #include "led.h"
 #include "timer.h"
 
+#define TIMER_OVF_PER_SECOND    (4U)   /* 4 x 262.144ms ~= 1.049s */
+
 static volatile unsigned char timer_ovf_count = 0;
 
 #pragma vector=TIMER0_OVF_vect
 __interrupt void timer0_ovf_isr(void)
 {
     timer_ovf_count++;
+
+    if (timer_ovf_count >= TIMER_OVF_PER_SECOND)
+    {
+        timer_ovf_count = 0;
+        led_toggle(LED0_PORT, LED0_PIN);
+    }
 }
 
 void main( void )
@@ -20,7 +28,7 @@ void main( void )
 
     timer_init();
     __enable_interrupt();
-    timer_start(TIMER_PRESCALER_1);
+    timer_start(TIMER_PRESCALER_1024);
 
     while(1)
     {
