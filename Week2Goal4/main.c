7e91991d651916eccc89255ebddf7ce0c5f66fd4
bianca.cotofana <biancamaria.cotofana@ulbsibiu.ro>
2026-07-22 19:45:38 +0300

    433 - CTC ~100ms waveform

--- Week2Goal4/main.c
+++ Week2Goal4/main.c
@@ -1,29 +1,18 @@
 #include "iom324pb.h"
-#include <intrinsics.h>
 #include "gpio.h"
 #include "led.h"
 #include "timer.h"
 
-#define TIMER_CTC_TOP   (127U)
-
-#pragma vector=TIMER0_COMPA_vect
-__interrupt void timer0_compa_isr(void)
-{
-    led_toggle(LED0_PORT, LED0_PIN);        
-}
+#define WAVE_TOP    (195U)   /* (195+1)*256us = 50.176ms per toggle -> ~100ms period */
 
 void main( void )
 {
     gpio_init();
+    led_init(LED4_DDR, LED4_PIN);        
 
-    led_init(LED4_DDR, LED4_PIN);           
-    led_init(LED0_DDR, LED0_PIN);
-    led_power_on(LED0_PORT, LED0_PIN);      
-
-    timer_init_ctc(TIMER_CTC_TOP);
+    timer_init_ctc(WAVE_TOP);
     timer_ctc_enable_oc0a_toggle();
-    __enable_interrupt();
-    timer_start(TIMER_PRESCALER_1024);
+    timer_start(TIMER_PRESCALER_256);
 
     while(1)
     {
