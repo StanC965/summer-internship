db935df1209fe65c71705ec1ae0882e711225e29
bianca.cotofana <biancamaria.cotofana@ulbsibiu.ro>
2026-07-22 19:59:34 +0300

    437 - 70/30 signal

--- Week2Goal4/main.c
+++ Week2Goal4/main.c
@@ -4,19 +4,29 @@
 #include "led.h"
 #include "timer.h"
 
-#define WAVE_TOP                (195U)   
-#define HALF_PERIOD_MATCHES     (4U)     
+#define WAVE_TOP            (195U)   
+#define HIGH_MATCHES        (14U)    
+#define LOW_MATCHES         (6U)    
 
 static volatile unsigned char match_count = 0;
+static volatile unsigned char led_is_high = 1;
 
 #pragma vector=TIMER0_COMPA_vect
 __interrupt void timer0_compa_isr(void)
 {
     match_count++;
-    if (match_count >= HALF_PERIOD_MATCHES)
+
+    if (led_is_high && (match_count >= HIGH_MATCHES))
     {
         match_count = 0;
-        led_toggle(LED0_PORT, LED0_PIN);
+        led_is_high = 0;
+        led_power_off(LED0_PORT, LED0_PIN);
+    }
+    else if (!led_is_high && (match_count >= LOW_MATCHES))
+    {
+        match_count = 0;
+        led_is_high = 1;
+        led_power_on(LED0_PORT, LED0_PIN);
     }
 }
 
@@ -24,7 +34,7 @@ void main( void )
 {
     gpio_init();
     led_init(LED0_DDR, LED0_PIN);
-    led_power_off(LED0_PORT, LED0_PIN);
+    led_power_on(LED0_PORT, LED0_PIN);  
 
     timer_init_ctc(WAVE_TOP);
     __enable_interrupt();
