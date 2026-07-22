3c03221f56f75f9fe12aae10cf5768d9371a739a
bianca.cotofana <biancamaria.cotofana@ulbsibiu.ro>
2026-07-22 19:51:12 +0300

    436

--- Week2Goal4/main.c
+++ Week2Goal4/main.c
@@ -1,17 +1,33 @@
 #include "iom324pb.h"
+#include <intrinsics.h>
 #include "gpio.h"
 #include "led.h"
 #include "timer.h"
 
-#define WAVE_TOP    (195U)   /* (195+1)*256us = 50.176ms per toggle -> ~100ms period */
+#define WAVE_TOP                (195U)   
+#define HALF_PERIOD_MATCHES     (4U)     
+
+static volatile unsigned char match_count = 0;
+
+#pragma vector=TIMER0_COMPA_vect
+__interrupt void timer0_compa_isr(void)
+{
+    match_count++;
+    if (match_count >= HALF_PERIOD_MATCHES)
+    {
+        match_count = 0;
+        led_toggle(LED0_PORT, LED0_PIN);
+    }
+}
 
 void main( void )
 {
     gpio_init();
-    led_init(LED4_DDR, LED4_PIN);        
+    led_init(LED0_DDR, LED0_PIN);
+    led_power_off(LED0_PORT, LED0_PIN);
 
     timer_init_ctc(WAVE_TOP);
-    timer_ctc_enable_oc0a_toggle();
+    __enable_interrupt();
     timer_start(TIMER_PRESCALER_256);
 
     while(1)
