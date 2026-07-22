e734c7803a1864d9c60bb6dcd9b0b364c725d44a
bianca.cotofana <biancamaria.cotofana@ulbsibiu.ro>
2026-07-22 20:02:14 +0300

    438 - 4segment signal

--- Week2Goal4/main.c
+++ Week2Goal4/main.c
@@ -4,29 +4,46 @@
 #include "led.h"
 #include "timer.h"
 
-#define WAVE_TOP            (195U)   
-#define HIGH_MATCHES        (14U)    
-#define LOW_MATCHES         (6U)    
+#define WAVE_TOP        (195U)   /
+#define WAVE_SEGMENTS   (4U)
 
+typedef struct
+{
+    unsigned char level;     
+    unsigned char matches;   
+} wave_segment_t;
+
+static const wave_segment_t wave[WAVE_SEGMENTS] =
+{
+    { 1, 4  },   /* 200ms HIGH */
+    { 0, 6  },   /* 300ms LOW  */
+    { 1, 10 },   /* 500ms HIGH */
+    { 0, 4  }    /* 200ms LOW  */
+};
+
+static volatile unsigned char seg_index = 0;
 static volatile unsigned char match_count = 0;
-static volatile unsigned char led_is_high = 1;
+
+static void wave_apply_level(unsigned char level)
+{
+    if (level)
+        led_power_on(LED0_PORT, LED0_PIN);
+    else
+        led_power_off(LED0_PORT, LED0_PIN);
+}
 
 #pragma vector=TIMER0_COMPA_vect
 __interrupt void timer0_compa_isr(void)
 {
     match_count++;
+    if (match_count >= wave[seg_index].matches)
+    {
+        match_count = 0;
+        seg_index++;
+        if (seg_index >= WAVE_SEGMENTS)
+            seg_index = 0;
 
-    if (led_is_high && (match_count >= HIGH_MATCHES))
-    {
-        match_count = 0;
-        led_is_high = 0;
-        led_power_off(LED0_PORT, LED0_PIN);
-    }
-    else if (!led_is_high && (match_count >= LOW_MATCHES))
-    {
-        match_count = 0;
-        led_is_high = 1;
-        led_power_on(LED0_PORT, LED0_PIN);
+        wave_apply_level(wave[seg_index].level);
     }
 }
 
@@ -34,7 +51,10 @@ void main( void )
 {
     gpio_init();
     led_init(LED0_DDR, LED0_PIN);
-    led_power_on(LED0_PORT, LED0_PIN);  
+
+    seg_index = 0;
+    match_count = 0;
+    wave_apply_level(wave[seg_index].level);  
 
     timer_init_ctc(WAVE_TOP);
     __enable_interrupt();
