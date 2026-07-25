0872ab63d39afec13a8121b226beb5829f7be685
bianca.cotofana <biancamaria.cotofana@ulbsibiu.ro>
2026-07-26 01:31:59 +0300

    Week3Goal5: new IAR project

---
+++ Week3Goal5/led.h
@@ -0,0 +1,68 @@
+#ifndef LED_H
+#define LED_H
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Includes
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+#include "gpio.h"
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Exported types and values
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+/** \brief  LED0 on the main board: PC7 */
+#define LED0_DDR            (&DDRC)
+#define LED0_PORT           (&PORTC)
+#define LED0_PIN            (7U)
+
+/** \brief  LED1 on OLED1 (EXT1): PD5 */
+#define LED1_DDR            (&DDRD)
+#define LED1_PORT           (&PORTD)
+#define LED1_PIN            (5U)
+
+/** \brief  LED2 on OLED1 (EXT1): PD4 */
+#define LED2_DDR            (&DDRD)
+#define LED2_PORT           (&PORTD)
+#define LED2_PIN            (4U)
+
+/** \brief  LED3 on OLED1 (EXT1): PA3 */
+#define LED3_DDR            (&DDRA)
+#define LED3_PORT           (&PORTA)
+#define LED3_PIN            (3U)
+
+/** \brief  LED4 on IO1 (EXT4): PB3 */
+#define LED4_DDR            (&DDRB)
+#define LED4_PORT           (&PORTB)
+#define LED4_PIN            (3U)
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+/*  Public functions            */
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+/** \brief  Initialize a LED pin as output
+    \param[in]  ddr  pointer to the DDRx register of the LED
+    \param[in]  pin  pin number of the LED
+*/
+extern void led_init(gpio_reg_t ddr, unsigned char pin);
+
+/** \brief  Turn a LED ON (all the LEDs are active low)
+    \param[in]  port  pointer to the PORTx register of the LED
+    \param[in]  pin   pin number of the LED
+*/
+extern void led_power_on(gpio_reg_t port, unsigned char pin);
+
+/** \brief  Turn a LED OFF
+    \param[in]  port  pointer to the PORTx register of the LED
+    \param[in]  pin   pin number of the LED
+*/
+extern void led_power_off(gpio_reg_t port, unsigned char pin);
+
+/** \brief  Toggle a LED */
+extern void led_toggle(gpio_reg_t port, unsigned char pin);
+
+/** \brief  Blink a LED fast: 6 on-and-off cycles per second */
+extern void led_blink_fast(gpio_reg_t port, unsigned char pin);
+
+/** \brief  Blink a LED slow: 2 on-and-off cycles per second */
+extern void led_blink_slow(gpio_reg_t port, unsigned char pin);
+
+#endif
---
+++ Week3Goal5/timer.c
@@ -0,0 +1,55 @@
+#ifndef TIMER_C
+#define TIMER_C
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Includes
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+#include "iom324pb.h"
+#include "timer.h"
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Static private objects
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+#define TIMER_TOIE0_BIT     (0U)
+#define TIMER_OCIE0A_BIT    (1U)
+#define TIMER_WGM01_BIT     (1U)
+#define TIMER_COM0A0_BIT    (6U)
+#define TIMER_CS_MASK       (0x07U)
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Implementation
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+void timer_init_normal(void)
+{
+    TCCR0A = 0x00;                             /* normal mode, no OC0x output */
+    TCCR0B = 0x00;                             /* WGM02 = 0, timer stopped */
+    TCNT0  = 0x00;
+    TIMSK0 |= (1 << TIMER_TOIE0_BIT);          /* overflow interrupt enable */
+}
+
+void timer_init_ctc(unsigned char top)
+{
+    TCCR0A = (1 << TIMER_WGM01_BIT);           /* CTC mode (WGM = 010), no OC0x output */
+    TCCR0B = 0x00;                             /* WGM02 = 0, timer stopped */
+    TCNT0  = 0x00;
+    OCR0A  = top;                              /* TOP value */
+    TIMSK0 |= (1 << TIMER_OCIE0A_BIT);         /* compare match A interrupt enable */
+}
+
+void timer_ctc_enable_oc0a_toggle(void)
+{
+    TCCR0A |= (1 << TIMER_COM0A0_BIT);        
+}
+
+void timer_start(unsigned char prescaler)
+{
+    TCCR0B = (TCCR0B & ~TIMER_CS_MASK) | (prescaler & TIMER_CS_MASK);
+}
+
+void timer_stop(void)
+{
+    TCCR0B &= ~TIMER_CS_MASK;
+}
+
+#endif
